b9931e280c2a422d655d57b6a33595c2bc784dee
kimxwan <kimxwan0319@naver.com>
2021-12-17 23:15:20 +0900

    ⚡️ :: Tuist Project 세팅

--- Application/Project.swift
+++ Application/Project.swift
@@ -1,1 +1,10 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
 
+let project = Project.excutable(
+    name: "XQUARE",
+    platform: .iOS,
+    dependencies: [
+        .project(target: "Service", path: "../Service")
+    ]
+)
