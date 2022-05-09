2a6b331b2192d500e73a006b49a01637406a5c74
kimxwan <kimxwan0319@naver.com>
2022-05-09 22:42:52 +0900

    ⚡️ :: KeychainModule

---
+++ Modules/KeychainModule/Sources/KeychainModule.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+import KeychainSwift
+
+public class KeychainModule<Key: KeychainKeyType>: KeychainSwift {
+
+    // MARK: - SET
+    @discardableResult
+    public func set(_ value: String, forKey key: Key, withAccess access: KeychainSwiftAccessOptions? = nil) -> Bool {
+        return super.set(value, forKey: key.keyString, withAccess: access)
+    }
+
+    @discardableResult
+    public func set(_ value: Data, forKey key: Key, withAccess access: KeychainSwiftAccessOptions? = nil) -> Bool {
+        return super.set(value, forKey: key.keyString, withAccess: access)
+    }
+
+    @discardableResult
+    public func set(_ value: Bool, forKey key: Key, withAccess access: KeychainSwiftAccessOptions? = nil) -> Bool {
+        return super.set(value, forKey: key.keyString, withAccess: access)
+    }
+
+    // MARK: - GET
+    public func get(_ key: Key) -> String? {
+        return super.get(key.keyString)
+    }
+
+    public func getData(_ key: Key, asReference: Bool = false) -> Data? {
+        return super.getData(key.keyString, asReference: asReference)
+    }
+
+    public func getBool(_ key: Key) -> Bool? {
+        return super.getBool(key.keyString)
+    }
+
+    // MARK: - GET
+    @discardableResult
+    public func delete(_ key: Key) -> Bool {
+        return super.delete(key.keyString)
+    }
+
+}
