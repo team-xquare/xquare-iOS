80e5cd69dbf4a69e1f5ae013e5d00bc4fa05fe52
kimxwan <kimxwan0319@naver.com>
2022-05-30 11:45:04 +0900

    ⚡️ :: LoaclTokenDataSource protocol

---
+++ Services/AuthService/Sources/Datasource/LocalDatasource/LoaclTokenDataSource.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+protocol LoaclTokenDataSource {
+    func registerAccessToken(_ accessToken: String)
+    func fetchAccessToken() -> String?
+    func registerRefreshToken(_ refreshToken: String)
+    func fetchRefreshToken() -> String?
+    func registerExpiredAt(_ expiredAt: String)
+    func fetchExpiredDate() -> Date
+}
