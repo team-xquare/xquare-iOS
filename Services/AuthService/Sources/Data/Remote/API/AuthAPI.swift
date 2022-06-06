1213090f51a8b9a757c8ae49e6b57da94d31f350
kimxwan <kimxwan0319@naver.com>
2022-06-06 23:48:44 +0900

    ⚡️ :: AuthAPI

---
+++ Services/AuthService/Sources/Data/Remote/API/AuthAPI.swift
@@ -0,0 +1,54 @@
+import Foundation
+
+import Moya
+
+enum AuthAPI: TargetType {
+    case signin(request: SigninRequest)
+    case signup(request: SignupRequest)
+    case refreshToken(refreshToken: String)
+}
+
+extension AuthAPI {
+    var baseURL: URL {
+        return URL(string: "https://staging-api.xquare.app")!
+    }
+    
+    var path: String {
+        switch self {
+        case .signin, .refreshToken:
+            return "/login"
+        case .signup:
+            return ""
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .signin, .signup:
+            return .post
+        case .refreshToken:
+            return .put
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .signin(let request):
+            return .requestJSONEncodable(request)
+        case .signup(let request):
+            return .requestJSONEncodable(request)
+        default:
+            return .requestPlain
+        }
+    }
+
+    var headers: [String: String]? {
+        switch self {
+        case .refreshToken(let refreshToken):
+            return ["Refresh-Token": "Bearer \(refreshToken)"]
+        default:
+            return nil
+        }
+    }
+
+}
