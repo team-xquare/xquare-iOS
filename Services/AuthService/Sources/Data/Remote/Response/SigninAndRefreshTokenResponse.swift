adf48f3b3cb88f25f6004a19507d315d786ef86d
kimxwan <kimxwan0319@naver.com>
2022-06-06 23:43:55 +0900

    ⚡️ :: SigninAndRefreshTokenResponse

---
+++ Services/AuthService/Sources/Data/Remote/Response/SigninAndRefreshTokenResponse.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct SigninAndRefreshTokenResponse: Decodable {
+
+    private enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case refreshToken = "refresh_token"
+        case expirationAt = "expiration_at"
+    }
+
+    let accessToken: String
+    let refreshToken: String
+    let expirationAt: String
+
+}
