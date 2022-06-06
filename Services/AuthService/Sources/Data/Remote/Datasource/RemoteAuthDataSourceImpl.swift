dbf0e817cb9dbfd0936b176c82c75e40e9858164
kimxwan <kimxwan0319@naver.com>
2022-06-06 23:49:48 +0900

    ⚡️ :: RemoteAuthDataSourceImpl

---
+++ Services/AuthService/Sources/Data/Remote/Datasource/RemoteAuthDataSourceImpl.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+import Moya
+import RxSwift
+import RxMoya
+
+class RemoteAuthDataSourceImpl: RemoteAuthDataSource {
+
+    private let provider = MoyaProvider<AuthAPI>()
+
+    func signin(request: SigninRequest) -> Single<SigninAndRefreshTokenResponse> {
+        self.provider.rx.request(.signin(request: request))
+            .map(SigninAndRefreshTokenResponse.self)
+    }
+
+    func signup(request: SignupRequest) -> Completable {
+        self.provider.rx.request(.signup(request: request))
+            .asCompletable()
+    }
+
+    func refreshToken(refreshToken: String) -> Single<SigninAndRefreshTokenResponse> {
+        self.provider.rx.request(.refreshToken(refreshToken: refreshToken))
+            .map(SigninAndRefreshTokenResponse.self)
+    }
+
+}
