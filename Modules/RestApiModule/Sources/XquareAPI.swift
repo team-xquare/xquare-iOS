00743838939547aa66e182b6c69864ae11d5a3bf
kimxwan <kimxwan0319@naver.com>
2022-06-07 15:39:44 +0900

    ⚡️ :: XquareAPI

---
+++ Modules/RestApiModule/Sources/XquareAPI.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+import Moya
+import AuthService
+
+protocol XquareAPI: TargetType, JWTTokenAuthorizable {
+    var domain: String { get }
+    var urlPath: String { get }
+    var errorMapper: [Int: Error]? { get }
+}
+
+extension XquareAPI {
+
+    var baseURL: URL { URL(string: "https://staging-api.xquare.app")! }
+
+    var path: String {
+        return domain+urlPath
+    }
+
+    var task: Task { .requestPlain }
+
+    var validationType: ValidationType {
+        return .successCodes
+    }
+
+    var headers: [String: String]? {
+        switch self {
+        default:
+            return ["Content-Type": "application/json"]
+        }
+    }
+
+}
