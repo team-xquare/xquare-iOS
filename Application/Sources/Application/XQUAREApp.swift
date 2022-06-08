6bc5ca53348650f4db84ed9a2c148725845abaa5
kimxwan <kimxwan0319@naver.com>
2022-06-08 14:33:57 +0900

    ⚡️ :: XQUAREApp에 assembler 추가

--- Application/Sources/Application/XQUAREApp.swift
+++ Application/Sources/Application/XQUAREApp.swift
@@ -1,7 +1,15 @@
 import SwiftUI
 
+import Swinject
+
+import AuthService
+
 @main
 struct XQUAREApp: App {
+
+    let assembler: Assembler = .init([
+        AuthServiceAssembly()
+    ])
 
     var body: some Scene {
         WindowGroup {
