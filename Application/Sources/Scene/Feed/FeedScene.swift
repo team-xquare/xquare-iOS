e06beabde25118f3b9411fec62178628521661bb
rlarldud1234 <rlarldud6482@naver.com>
2022-07-01 22:18:31 +0900

    ⚡️ :: FeedScene TabView

---
+++ Application/Sources/Scene/Feed/FeedScene.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct FeedScene: View {
+    var body: some View {
+        Text("피드 화면")
+    }
+}
+
+struct FeedScene_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedScene()
+    }
+}
