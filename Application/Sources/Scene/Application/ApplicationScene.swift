f7dc8db3f88bb63fb3fc18c47990d5a0e5dde2b8
rlarldud1234 <rlarldud6482@naver.com>
2022-07-01 22:17:15 +0900

    ⚡️ :: Tabbar ApplicationScene

---
+++ Application/Sources/Scene/Application/ApplicationScene.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ApplicationScene: View {
+    var body: some View {
+        Text("신청 화면")
+    }
+}
+
+struct ApplicationScene_Previews: PreviewProvider {
+    static var previews: some View {
+        ApplicationScene()
+    }
+}
