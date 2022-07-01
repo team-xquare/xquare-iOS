55c2cd561c4333f063da5c090135ffbf89dd590e
rlarldud1234 <rlarldud6482@naver.com>
2022-07-01 22:18:49 +0900

    ⚡️ :: HomeScene TabView

---
+++ Application/Sources/Scene/Home/HomeScene.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct HomeScene: View {
+    var body: some View {
+        Text("홈 화면")
+    }
+}
+
+struct HomeScene_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScene()
+    }
+}
