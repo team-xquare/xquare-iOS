fce11dec52396e6cb16ca2a255a5e72110e24d8d
rlarldud1234 <rlarldud6482@naver.com>
2022-07-01 23:29:25 +0900

    ⚡️ :: Scene을 View로 네이밍 변경

---
+++ Application/Sources/Scene/Entire/EntireView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct EntireView: View {
+    var body: some View {
+        Text("전체 화면")
+    }
+}
+
+struct EntireView_Previews: PreviewProvider {
+    static var previews: some View {
+        EntireView()
+    }
+}
---
+++ Application/Sources/Scene/Home/HomeView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("홈 화면")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
