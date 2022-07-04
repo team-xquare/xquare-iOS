42dd5c22e7e646a5d7e40f093893e2863e8c94d9
rlarldud1234 <rlarldud6482@naver.com>
2022-07-04 12:15:06 +0900

    ⚡️ :: FetchMonthToMealMenuUseCase

---
+++ Services/MealDataService/Sources/Domain/UseCase/FetchMonthToMealMenuUseCase.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+import RxSwift
+
+public class FetchMonthToMealMenuUseCase {
+
+    private let mealRepository: MealRepository
+
+    init(mealRepository: MealRepository) {
+        self.mealRepository = mealRepository
+    }
+
+    func excute(
+        year: String,
+        month: String
+    ) -> Observable<[MonthToMealMenuEntity]> {
+        return mealRepository.fetchMonthtoMealMenu(
+            year: year,
+            month: month
+        )
+    }
+}
