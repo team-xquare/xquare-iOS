9e429276e03682c2459d9c5ec5ba9ec47951d3ca
rlarldud1234 <rlarldud6482@naver.com>
2022-07-04 12:10:49 +0900

    ⚡️ :: RemoteDataSource

---
+++ Services/MealDataService/Sources/Data/Remote/DateSource/RemoteDataSource.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+import RxSwift
+
+protocol RemoteDataSource {
+    func fetchDayToMealMenu(date: String) -> Observable<DayToMealMenuEntity>
+    func fetchMonthToMealMenu(year: String, month: String) -> Observable<[MonthToMoealMenuEntity]>
+}
