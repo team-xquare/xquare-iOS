f46e7632ebf06528338162110d951e0c93bd8f3b
rlarldud1234 <rlarldud6482@naver.com>
2022-07-04 12:12:03 +0900

    ⚡️ :: MonthToMealMenuResponse

---
+++ Services/MealDataService/Sources/Data/Remote/Response/MonthToMealMenuResponse.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+struct MonthToMealMenuResponse: Decodable {
+
+    private enum CodingKeys: String, CodingKey {
+        case date
+        case breakfast
+        case lunch
+        case dinner
+    }
+
+    let date: String
+    let breakfast: [String]
+    let lunch: [String]
+    let dinner: [String]
+}
+
+extension MonthToMealMenuResponse {
+    func toDomain() -> MonthToMoealMenuEntity {
+        return .init(
+            date: date,
+            breakfast: breakfast,
+            lunch: lunch,
+            dinner: dinner
+        )
+    }
+}
