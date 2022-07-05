7f00cae8e3173bb34384587e22763da90cf6f58b
rlarldud1234 <rlarldud6482@naver.com>
2022-07-05 21:46:23 +0900

    ⚰️ :: 네이밍 변경으로 인한 파일 삭제

--- Services/MealDataService/Sources/Data/Remote/DateSource/RemoteDataSource.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-import RxSwift
-
-protocol RemoteDataSource {
-    func fetchDayToMealMenu(date: String) -> Observable<DayToMealMenuEntity>
-    func fetchMonthToMealMenu(year: String, month: String) -> Observable<[MonthToMealMenuEntity]>
-}
