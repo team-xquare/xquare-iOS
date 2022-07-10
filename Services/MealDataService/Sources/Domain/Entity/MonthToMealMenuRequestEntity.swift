b038643fd7b35d884a68a499adf3b7729b79ea93
rlarldud1234 <rlarldud6482@naver.com>
2022-07-10 20:17:53 +0900

    ⚡️ :: 파라미터 타입 String -> Date로 변경

--- Services/MealDataService/Sources/Domain/Entity/MonthToMealMenuRequestEntity.swift
+++ Services/MealDataService/Sources/Domain/Entity/MonthToMealMenuRequestEntity.swift
@@ -1,15 +1,15 @@
 import Foundation
 
 public struct MonthToMealMenuRequestEntity {
-    let year: String
-    let month: String
+    let year: Date
+    let month: Date
 }
 
 extension MonthToMealMenuRequestEntity {
     func toMonthToMealMenuRequest() -> MonthToMealMenuRequest {
         return .init(
-            year: self.year,
-            month: self.month
+            year: self.year.toYearString(),
+            month: self.month.toMonthString()
         )
     }
 }
