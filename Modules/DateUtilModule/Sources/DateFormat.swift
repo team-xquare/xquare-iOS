071dcaa741b71756a37dfecb9fd7441e431a27d3
kimxwan <kimxwan0319@naver.com>
2022-07-14 14:59:41 +0900

    ⚡️ :: DateFormat

---
+++ Modules/DateUtilModule/Sources/DateFormat.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public enum DateFormat: String {
+    /// yyyy-MM-dd'T'HH:mm:ss
+    case fullDateWithTime = "yyyy-MM-dd'T'HH:mm:ss"
+    /// yyyy-MM-dd
+    case fullDate =  "yyyy-MM-dd"
+}
