beef54b3c8150fc7b8e511b1d16572aa461d0872
kimxwan <kimxwan0319@naver.com>
2022-07-14 15:00:24 +0900

    🧪 :: DateToStringSpec

---
+++ Modules/DateUtilModule/Tests/DateToStringSpec.swift
@@ -0,0 +1,28 @@
+import Foundation
+import Quick
+import Nimble
+
+@testable import DateUtilModule
+
+class DateToStringSpec: QuickSpec {
+
+    var mockDate: Date!
+
+    override func spec() {
+        describe("특정 Date를") {
+            beforeEach {
+                var dateComponents = DateComponents()
+                dateComponents.year = 2022
+                dateComponents.month = 7
+                dateComponents.day = 14
+                self.mockDate = Calendar.current.date(from: dateComponents)
+            }
+            context("String 값으로 바꿀 때") {
+                it("지정된 Format으로 변환 되어야 한다.") {
+                    expect(self.mockDate.toString(format: .fullDate)) == "2022-07-14"
+                }
+            }
+        }
+    }
+
+}
