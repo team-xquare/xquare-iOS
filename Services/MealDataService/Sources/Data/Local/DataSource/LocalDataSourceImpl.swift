eb33003248eeba3f71c4526817109b68831ad4b1
rlarldud1234 <rlarldud6482@naver.com>
2022-07-14 19:04:30 +0900

    ⚡️ :: LocalDataSourceImpl

--- Services/MealDataService/Sources/Data/Local/DataSource/LocalDataSourceImpl.swift
+++ Services/MealDataService/Sources/Data/Local/DataSource/LocalDataSourceImpl.swift
@@ -5,19 +5,19 @@ class LocalDataSourceImpl: LocalDataSource {
     let mealDataSQLiteTask = MealDataServiceSQLiteTask.shared
 
     func registerDayToMealMenu(day: Date, breakfast: [String], lunch: [String], dinner: [String]) {
-        mealDataSQLiteTask.insertData(
-            date: day,
+        mealDataSQLiteTask.save(mealMenu: MealMenu(
+            day: day,
             breakfast: breakfast.joined(separator: " "),
             lunch: lunch.joined(separator: " "),
             dinner: dinner.joined(separator: " ")
-        )
+        ))
     }
 
-    func fetchDayToMealMenu(day: Date) -> DayToMealMenuEntity {
-        return mealDataSQLiteTask.readData(day: day)
+    func fetchMealMenuPerDay(day: Date) -> DayToMealMenuEntity {
+        return mealDataSQLiteTask.findMealByDay(day: day)
     }
 
-    func fetchMonthToMealMenu(day: Date) -> [MonthToMealMenuEntity] {
-        return mealDataSQLiteTask.readMonthToMealMenu(day: day)
+    func fetchMealMenuPerMonth(day: Date) -> [MonthToMealMenuEntity] {
+        return mealDataSQLiteTask.findMealByMonth(day: day)
     }
 }
