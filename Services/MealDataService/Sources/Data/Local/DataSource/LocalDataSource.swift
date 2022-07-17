ffb061b46ef6a8969cdd80d7146d9abe5f4d9759
rlarldud1234 <rlarldud6482@naver.com>
2022-07-17 19:35:09 +0900

    ⚡️ :: registerMonthToMealMenu

--- Services/MealDataService/Sources/Data/Local/DataSource/LocalDataSource.swift
+++ Services/MealDataService/Sources/Data/Local/DataSource/LocalDataSource.swift
@@ -4,6 +4,7 @@ import RxSwift
 
 protocol LocalDataSource {
     func registerDayToMealMenu(day: Date, breakfast: [String], lunch: [String], dinner: [String])
+    func registerMonthToMealMenu(menu: [MonthToMealMenuEntity])
     func fetchMealMenuPerDay(day: Date) -> Single<DayToMealMenuEntity>
     func fetchMealMenuPerMonth(day: Date) -> Single<[MonthToMealMenuEntity]>
 }
