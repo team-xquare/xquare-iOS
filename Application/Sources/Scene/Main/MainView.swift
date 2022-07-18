0f17ca482aeb2f2b03077f9cd4aed260e0389006
kimxwan <kimxwan0319@naver.com>
2022-07-18 18:30:28 +0900

    ♻️ :: 탭바 아이콘 변경

--- Application/Sources/Scene/Main/MainView.swift
+++ Application/Sources/Scene/Main/MainView.swift
@@ -13,27 +13,27 @@ struct MainView: View {
         TabView {
             homeView
                 .tabItem {
-                    Image(systemName: "clock.fill")
+                    Image(systemName: "square.fill")
                     SDText("홈", type: .body3)
                 }
             scheduleView
                 .tabItem {
-                    Image(systemName: "clock.fill")
+                    Image(systemName: "square.fill")
                     SDText("일정", type: .body3)
                 }
             feedView
                 .tabItem {
-                    Image(systemName: "clock.fill")
+                    Image(systemName: "square.fill")
                     SDText("피드", type: .body3)
                 }
             applicationView
                 .tabItem {
-                    Image(systemName: "clock.fill")
+                    Image(systemName: "square.fill")
                     SDText("신청", type: .body3)
                 }
             entireView
                 .tabItem {
-                    Image(systemName: "clock.fill")
+                    Image(systemName: "square.fill")
                     SDText("전체", type: .body3)
                 }
         }
