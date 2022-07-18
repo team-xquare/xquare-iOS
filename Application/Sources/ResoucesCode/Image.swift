3f63cacda4f85d9212a330a0c88c610c2408eaa7
kimxwan <kimxwan0319@naver.com>
2022-07-19 01:58:46 +0900

    ⚡️ :: 리소스 코드 추가

--- Application/Sources/ResoucesCode/Image.swift
+++ Application/Sources/ResoucesCode/Image.swift
@@ -1,5 +1,13 @@
 import SwiftUI
 
 extension Image {
+    struct OnboardingImages { }
+}
+
+extension Image {
     static let logo = Image("Logo")
+}
+
+extension Image.OnboardingImages {
+    static let dms = Image("Onboarding-DMS")
 }
