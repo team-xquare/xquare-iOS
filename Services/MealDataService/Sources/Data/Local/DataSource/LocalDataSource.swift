12042182898a8e6472410a0805b9e92125e30699
rlarldud1234 <rlarldud6482@naver.com>
2022-07-26 23:43:10 +0900

    ⚡️ :: Data Type 변경하기

--- Services/MealDataService/Sources/Data/Local/DataSource/LocalDataSource.swift
+++ Services/MealDataService/Sources/Data/Local/DataSource/LocalDataSource.swift
@@ -3,7 +3,7 @@ import Foundation
 import RxSwift
 
 protocol LocalDataSource {
-    func registerMealMenuPerDay(day: Date, breakfast: [String], lunch: [String], dinner: [String])
+    func registerMealMenuPerDay(menu: [MealMenuEntity])
     func registerMealMenuPerMonth(menu: [[MealMenuEntity]])
     func fetchMealMenuPerDay(day: Date) -> Single<MealMenuPerDayEntity>
     func fetchMealMenuPerMonth(day: Date) -> Single<[[MealMenuEntity]]>
