592290e54596d574dcc9fa3ee32902f0cc2b6545
kimxwan <kimxwan0319@naver.com>
2022-08-06 17:56:27 +0900

    ⚡️ :: XWebKitState 재정의

--- Modules/XWebKit/Sources/XWebKitState.swift
+++ Modules/XWebKit/Sources/XWebKitState.swift
@@ -5,18 +5,33 @@ import RxCocoa
 
 class XWebKitState: ObservableObject {
 
+    @Published var title: String = ""
+    @Published var urlString: String = ""
+
     @Published var loadingProgress: Double = 0.0
 
-    @Published var isNavigated: Bool = false
+    @Published var needsToNavigate: Bool = false
     @Published var naviagteLink: String = ""
 
     @Published var isImageViewerPresented: Bool = false
     @Published var images: [URL] = []
+
+    var isPresentated: Binding<Bool>
 
     @Published var isAlertPresented: Bool = false
     @Published var alertMessage: String = ""
     @Published var alertConfirmText: String = ""
     @Published var alertCancelText: String = ""
     var alertResponse = PublishSubject<Bool>()
+
+    init(
+        title: String,
+        urlString: String,
+        isPresentated: Binding<Bool>
+    ) {
+        self.title = title
+        self.urlString = urlString
+        self.isPresentated = isPresentated
+    }
 
 }
