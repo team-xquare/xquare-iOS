f4958970414fad1ee326a394e381314e42d34de6
rlarldud1234 <rlarldud6482@naver.com>
2022-08-06 14:13:30 +0900

    ⚡️ :: FindIdView

---
+++ Application/Sources/Scene/FindId/FindIdView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct FindIdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FindIdView_Previews: PreviewProvider {
+    static var previews: some View {
+        FindIdView()
+    }
+}
