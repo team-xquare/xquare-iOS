e3dad5c072424f66ef5da7d574d5af939d6ffb91
rlarldud1234 <rlarldud6482@naver.com>
2022-08-06 14:13:51 +0900

    ⚡️ :: FindPasswordView

---
+++ Application/Sources/Scene/FindPassword/FindPasswordView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct FindPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FindPasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        FindPasswordView()
+    }
+}
