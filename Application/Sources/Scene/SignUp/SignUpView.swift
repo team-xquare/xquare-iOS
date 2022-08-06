10f24f04dfec96be64e87a4c030f64833120c9ef
rlarldud1234 <rlarldud6482@naver.com>
2022-08-06 18:59:45 +0900

    ⚡️ :: ScrollView 추가

--- Application/Sources/Scene/SignUp/SignUpView.swift
+++ Application/Sources/Scene/SignUp/SignUpView.swift
@@ -6,38 +6,40 @@ struct SignUpView: View {
     @StateObject var viewModel: SignUpViewModel
     var body: some View {
         NavigationView {
-            VStack(spacing: 20) {
-                Spacer()
-                    .frame(height: 16)
-                SDTextField(
-                    title: "인증코드",
-                    placeholder: "6자리를 입력해주세요",
-                    text: $viewModel.authCode
-                ).padding(.horizontal, 16)
-                SDTextField(
-                    title: "아이디",
-                    placeholder: "영문, 숫자 6~20자",
-                    text: $viewModel.id
-                ).padding(.horizontal, 16)
-                SDTextField(
-                    title: "비밀번호",
-                    placeholder: "숫자, 영문, 특수문자 조합 최소 6자",
-                    text: $viewModel.password
-                ).padding(.horizontal, 16)
-                SDTextField(
-                    title: "비밀번호 재입력",
-                    placeholder: "재입력",
-                    text: $viewModel.reEnterPassword
-                ).padding(.horizontal, 16)
-                Spacer()
-                FillButton(
-                    text: "입력 완료",
-                    action: {
-                        print("회원가입")
-                    },
-                    type: .rounded
-                )
-            }.navigationTitle("회원가입")
+            ScrollView {
+                VStack(spacing: 20) {
+                    Spacer()
+                        .frame(height: 16)
+                    SDTextField(
+                        title: "인증코드",
+                        placeholder: "6자리를 입력해주세요",
+                        text: $viewModel.authCode
+                    ).padding(.horizontal, 16)
+                    SDTextField(
+                        title: "아이디",
+                        placeholder: "영문, 숫자 6~20자",
+                        text: $viewModel.id
+                    ).padding(.horizontal, 16)
+                    SDTextField(
+                        title: "비밀번호",
+                        placeholder: "숫자, 영문, 특수문자 조합 최소 6자",
+                        text: $viewModel.password
+                    ).padding(.horizontal, 16)
+                    SDTextField(
+                        title: "비밀번호 재입력",
+                        placeholder: "재입력",
+                        text: $viewModel.reEnterPassword
+                    ).padding(.horizontal, 16)
+                    Spacer()
+                    FillButton(
+                        text: "입력 완료",
+                        action: {
+                            print("회원가입")
+                        },
+                        type: .rounded
+                    )
+                }.navigationTitle("회원가입")
+            }
         }
     }
 }
@@ -46,7 +48,7 @@ struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
         let viewModel = SignUpViewModel()
         SignUpView(
-        viewModel: viewModel
+            viewModel: viewModel
         )
     }
 }
