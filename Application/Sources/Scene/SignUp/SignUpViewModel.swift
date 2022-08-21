fb86d41ef84353156737ec245b77e8b67c13a38d
rlarldud1234 <rlarldud6482@naver.com>
2022-08-21 20:35:27 +0900

    ⚡️ :: SignUp

--- Application/Sources/Scene/SignUp/SignUpViewModel.swift
+++ Application/Sources/Scene/SignUp/SignUpViewModel.swift
@@ -1,8 +1,21 @@
 import Foundation
 
+import AuthService
+import RxSwift
+
 class SignUpViewModel: ObservableObject {
     @Published var authCode: String = ""
     @Published var id: String = ""
     @Published var password: String = ""
     @Published var reEnterPassword: String = ""
+
+    private let signupuseCase: SignupUseCase
+
+    init(signupUseCase: SignupUseCase) {
+        self.signupuseCase = signupUseCase
+    }
+
+    func signUp() {
+        self.signupuseCase.excute(data: .init(authCode: authCode, id: id, password: password))
+    }
 }
