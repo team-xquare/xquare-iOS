705bf91bafcc8e3434e6450a3e608e6d308c82bb
rlarldud1234 <rlarldud6482@naver.com>
2022-08-22 14:03:48 +0900

    ⚡️ :: SignUpViewModel

--- Application/Sources/Scene/SignUp/SignUpViewModel.swift
+++ Application/Sources/Scene/SignUp/SignUpViewModel.swift
@@ -8,14 +8,26 @@ class SignUpViewModel: ObservableObject {
     @Published var id: String = ""
     @Published var password: String = ""
     @Published var reEnterPassword: String = ""
+    @Published var isSuccess: Bool = false
 
     private let signupuseCase: SignupUseCase
+
+    private var disposeBag = DisposeBag()
 
     init(signupUseCase: SignupUseCase) {
         self.signupuseCase = signupUseCase
     }
 
     func signUp() {
-        self.signupuseCase.excute(data: .init(authCode: authCode, id: id, password: password))
+        self.signupuseCase.excute(data: .init(
+            authCode: authCode,
+            id: id,
+            profileImageUrl: nil,
+            password: password
+        )).subscribe {
+            self.isSuccess = true
+        } onError: { _ in
+            self.isSuccess = false
+        }.disposed(by: disposeBag)
     }
 }
