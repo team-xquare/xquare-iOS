abede51e531edb4b44576a2313a5d6712fdb844e
rlarldud1234 <rlarldud6482@naver.com>
2022-08-24 00:41:05 +0900

    ⚡️ :: UseCase 적용 및 에러 메시지

--- Application/Sources/Scene/SignUp/SignUpViewModel.swift
+++ Application/Sources/Scene/SignUp/SignUpViewModel.swift
@@ -8,7 +8,10 @@ class SignUpViewModel: ObservableObject {
     @Published var id: String = ""
     @Published var password: String = ""
     @Published var reEnterPassword: String = ""
+    @Published var passwordErrorMessage: String = ""
+    @Published var idErrorMessage: String = ""
     @Published var isSuccess: Bool = false
+    @Published var isDisabled: Bool = true
 
     private let signupuseCase: SignupUseCase
 
@@ -24,10 +27,54 @@ class SignUpViewModel: ObservableObject {
             id: id,
             profileImageUrl: nil,
             password: password
-        )).subscribe {
-            self.isSuccess = true
-        } onError: { _ in
-            self.isSuccess = false
-        }.disposed(by: disposeBag)
+        ))
+        .subscribe(onCompleted: { [weak self] in
+            self?.isSuccess = false
+        }, onError: { [weak self] in
+            if $0.asAuthServiceError == .duplicateId {
+                self?.idErrorMessage = "아이디가 중복되었습니다."
+            }
+            self?.isSuccess = false
+        })
+        .disposed(by: self.disposeBag)
+    }
+
+    func checkSignup() {
+        self.isDisabled = !isCheckAuthCode() || !isIdCheck() || !isPasswordCheck() || !isReEnterPasswordCheck()
+    }
+
+    func equalPasswordError() {
+        if !isReEnterPasswordCheck() {
+            self.passwordErrorMessage = "비밀번호가 일치하지 않습니다."
+        } else {
+            self.passwordErrorMessage = ""
+        }
+    }
+
+    private func isCheckAuthCode() -> Bool {
+        return authCode.count == 6
+    }
+
+    private func isIdCheck() -> Bool {
+        let strRegEx = "[A-Za-z0-9]{6,20}"
+        let pred = NSPredicate(format: "SELF MATCHES %@", strRegEx)
+
+        return pred.evaluate(with: self.id)
+    }
+
+    private func isPasswordCheck() -> Bool {
+        let strRegEx = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{6,30}"
+        let pred = NSPredicate(format: "SELF MATCHES %@", strRegEx)
+
+        return pred.evaluate(with: self.password)
+    }
+
+    private func isReEnterPasswordCheck() -> Bool {
+        return self.password == self.reEnterPassword
     }
 }
+
+/*
+ * 409일때 아이디 오류
+    - 아이디가 중복되었습니다.
+ */
