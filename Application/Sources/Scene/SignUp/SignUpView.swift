33140ff52742c94d64e453974f2dcca85880c7d6
rlarldud1234 <rlarldud6482@naver.com>
2022-08-24 00:40:35 +0900

    ⚡️ :: TextField onChange 적용

--- Application/Sources/Scene/SignUp/SignUpView.swift
+++ Application/Sources/Scene/SignUp/SignUpView.swift
@@ -15,40 +15,58 @@ struct SignUpView: View {
                             title: "인증코드",
                             placeholder: "6자리를 입력해주세요",
                             text: $viewModel.authCode
-                        ).padding(.horizontal, 16)
+                        )
+                        .padding(.horizontal, 16)
+                        .onChange(of: viewModel.authCode) { _ in
+                            viewModel.checkSignup()
+                        }
                         SDTextField(
                             title: "아이디",
                             placeholder: "영문, 숫자 6~20자",
-                            text: $viewModel.id
-                        ).padding(.horizontal, 16)
+                            text: $viewModel.id,
+                            errorMessage: $viewModel.idErrorMessage
+                        )
+                        .padding(.horizontal, 16)
+                        .onChange(of: viewModel.id) { _ in
+                            viewModel.checkSignup()
+                        }
                         SDTextField(
                             title: "비밀번호",
                             placeholder: "숫자, 영문, 특수문자 조합 최소 6자",
                             text: $viewModel.password
-                        ).padding(.horizontal, 16)
+                        )
+                        .padding(.horizontal, 16)
+                        .onChange(of: viewModel.password) { _ in
+                            viewModel.equalPasswordError()
+                            viewModel.checkSignup()
+                        }
                         SDTextField(
                             title: "비밀번호 재입력",
                             placeholder: "재입력",
                             text: $viewModel.reEnterPassword,
-                            isDisableed: .constant(viewModel.password != viewModel.reEnterPassword)
-                        ).padding(.horizontal, 16)
+                            errorMessage: $viewModel.passwordErrorMessage
+                        )
+                        .padding(.horizontal, 16)
+                        .onChange(of: viewModel.reEnterPassword) { _ in
+                            viewModel.equalPasswordError()
+                            viewModel.checkSignup()
+                        }
                         Spacer()
                     }
                 }
                 VStack {
                     Spacer()
-                    NavigationLink(
-                        isActive: $viewModel.isSuccess) {
-                            AppDependency.resolve().loginView
-                        } label: {
-                            FillButton(
-                                text: "입력 완료",
-                                action: {
-                                    viewModel.signUp()
-                                },
-                                type: .rounded
-                            )
-                        }
+                    FillButton(
+                        isDisabled: $viewModel.isDisabled,
+                        text: "입력 완료",
+                        action: {
+                            viewModel.signUp()
+                        },
+                        type: .rounded
+                    )
+                    .fullScreenCover(isPresented: $viewModel.isSuccess) {
+                        AppDependency.resolve().loginView
+                    }
                 }
             }.navigationTitle("회원가입")
         }
