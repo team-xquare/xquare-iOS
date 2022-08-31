140eedff9c21be7caf49b05b02ad4408aa9d75d0
rlarldud1234 <rlarldud6482@naver.com>
2022-08-31 16:51:34 +0900

    ⚡️ :: LoginViewModel UseCase 연동하기

--- Application/Sources/Scene/Login/LoginViewModel.swift
+++ Application/Sources/Scene/Login/LoginViewModel.swift
@@ -1,6 +1,40 @@
 import SwiftUI
 
+import AuthService
+import RxSwift
+
 class LoginViewModel: ObservableObject {
     @Published var id: String = ""
     @Published var password: String = ""
+    @Published var errorMessage: String = ""
+    @Published var isSuccess: Bool = false
+
+    private let signInUseCase: SigninUseCase
+
+    private var disposeBag = DisposeBag()
+
+    init(signInUseCase: SigninUseCase) {
+        self.signInUseCase = signInUseCase
+    }
+
+    func textFieldIsEmpty() -> Bool {
+        return id.isEmpty || password.isEmpty
+    }
+
+    func login() {
+        signInUseCase.excute(
+            data: .init(
+                id: id,
+                password: password,
+                deviceToken: "dsfisdofds"
+            ))
+        .subscribe(onCompleted: { [weak self] in
+            self?.isSuccess = true
+        }, onError: { [weak self] in
+            if $0.asAuthServiceError == .failToSignin {
+                self?.errorMessage = "아이디나 비밀번호가 일치하지 않습니다."
+            }
+            self?.isSuccess = false
+        }).disposed(by: disposeBag)
+    }
 }
