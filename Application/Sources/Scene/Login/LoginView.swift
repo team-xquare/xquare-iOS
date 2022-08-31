c149e3ff3b79eefa174a35ddb1f7c6cbf5b118d1
rlarldud1234 <rlarldud6482@naver.com>
2022-08-31 16:51:16 +0900

    ⚡️ :: LoginView 뷰모델 연동

--- Application/Sources/Scene/Login/LoginView.swift
+++ Application/Sources/Scene/Login/LoginView.swift
@@ -1,8 +1,12 @@
 import SwiftUI
 import SemicolonDesign
+import AuthService
 
 struct LoginView: View {
     @StateObject var viewModel: LoginViewModel
+    @State var isDisabled: Bool = true
+    var mainView: MainView!
+
     var body: some View {
         NavigationView {
             VStack {
@@ -11,19 +15,35 @@ struct LoginView: View {
                 SDTextField(
                     placeholder: "아이디",
                     text: $viewModel.id
-                ).padding(.horizontal, 16)
+                )
+                .onChange(
+                    of: viewModel.id,
+                    perform: { _ in
+                        isDisabled = viewModel.textFieldIsEmpty()
+                    })
+                .padding(.horizontal, 16)
                 SDTextField(
                     placeholder: "비밀번호",
-                    text: $viewModel.password
+                    text: $viewModel.password,
+                    errorMessage: viewModel.errorMessage
                 )
+                .onChange(
+                    of: viewModel.password,
+                    perform: { _ in
+                        isDisabled = viewModel.textFieldIsEmpty()
+                    })
                 .padding(.horizontal, 16)
                 FillButton(
+                    isDisabled: $isDisabled,
                     text: "로그인",
                     action: {
-                        print("로그인")
+                        viewModel.login()
                     },
                     type: .rounded
                 )
+                .fullScreenCover(isPresented: $viewModel.isSuccess) {
+                    mainView
+                }
                 FindButtonView()
                 Spacer()
             }
@@ -34,7 +54,6 @@ struct LoginView: View {
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        let viewModel = LoginViewModel()
-        LoginView(viewModel: viewModel)
+        LoginView(viewModel: LoginViewModel(signInUseCase: AuthServiceDependency.resolve().signinUseCase))
     }
 }
