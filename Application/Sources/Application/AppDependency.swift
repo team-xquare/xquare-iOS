58f0fa6a660ba255ee44594c002e6407568cfeae
rlarldud1234 <rlarldud6482@naver.com>
2022-08-31 16:50:54 +0900

    ⚡️ :: AppDependency

--- Application/Sources/Application/AppDependency.swift
+++ Application/Sources/Application/AppDependency.swift
@@ -10,9 +10,16 @@ struct AppDependency {
 extension AppDependency {
     static func resolve() -> AppDependency {
         let dependency = MealDataServiceDependency.resolve()
+        let authDependency = AuthServiceDependency.resolve()
         // MARK: - ViewModels
         let homeViewModel = HomeViewModel(
             fetchMealMenuPerDayUseCase: dependency.fetchDayToMealMenuUseCase
+        )
+        let loginViewModel = LoginViewModel(
+            signInUseCase: authDependency.signinUseCase
+        )
+        let signUpViewModel = SignUpViewModel(
+            signupUseCase: authDependency.signupUseCase
         )
 
         // MARK: - Views
@@ -29,7 +36,14 @@ extension AppDependency {
             applicationView: applicationView,
             entireView: entireView
         )
-
+        let loginView = LoginView(
+            viewModel: loginViewModel,
+            mainView: mainView
+        )
+        _ = SignUpView(
+            viewModel: signUpViewModel,
+            loginView: loginView
+        )
         return AppDependency(
             mainView: mainView
         )
