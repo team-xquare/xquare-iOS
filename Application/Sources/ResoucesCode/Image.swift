53e76d5fdd2170fcede7664115681144f028e101
kimxwan <kimxwan0319@naver.com>
2022-09-01 10:09:10 +0900

    📑 :: DSM 온보딩 이미지 추가

--- Application/Sources/ResoucesCode/Image.swift
+++ Application/Sources/ResoucesCode/Image.swift
@@ -1,5 +1,13 @@
 import SwiftUI
 
 extension Image {
+    struct OnboardingImage {}
+}
+
+extension Image {
     static let logo = Image("Logo")
+}
+
+extension Image.OnboardingImage {
+    static let dms = Image("Onboarding-DMS")
 }
