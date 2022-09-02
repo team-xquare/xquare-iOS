7c7268dfcb2f7dfdae88ccd499e43dea7ce65f30
kimxwan <kimxwan0319@naver.com>
2022-09-02 13:41:39 +0900

    ♻️ :: 리소스 코드 구조 변경

--- Application/Sources/ResoucesCode/Image.swift
+++ Application/Sources/ResoucesCode/Image.swift
@@ -1,13 +1,9 @@
 import SwiftUI
 
 extension Image {
-    struct OnboardingImage {}
-}
 
-extension Image {
     static let logo = Image("Logo")
-}
 
-extension Image.OnboardingImage {
-    static let dms = Image("Onboarding-DMS")
+    static let onboardingDms = Image("Onboarding-DMS")
+
 }
