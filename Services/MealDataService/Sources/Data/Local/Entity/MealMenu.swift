14d8cd577494fad238ee793d319e165e0a40c5a1
rlarldud1234 <rlarldud6482@naver.com>
2022-09-07 13:28:03 +0900

    ⚡️ :: MealMenu + toDomain

--- Services/MealDataService/Sources/Data/Local/Entity/MealMenu.swift
+++ Services/MealDataService/Sources/Data/Local/Entity/MealMenu.swift
@@ -6,3 +6,16 @@ struct MealMenu: Equatable {
     let lunch: String
     let dinner: String
 }
+
+extension MealMenu {
+    func toDomain() -> MealMenuPerDayEntity {
+        return .init(
+            date: day,
+            menu: [
+                .init(mealTime: .breakfast, menu: breakfast.components(separatedBy: " ")),
+                .init(mealTime: .lunch, menu: lunch.components(separatedBy: " ")),
+                .init(mealTime: .dinner, menu: dinner.components(separatedBy: " "))
+            ]
+        )
+    }
+}
