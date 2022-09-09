967e10547b3fea280aea881ffb92d1d3e5dfa8ae
rlarldud1234 <rlarldud6482@naver.com>
2022-09-09 21:10:40 +0900

    ♻️ :: null 처리해주기

--- Services/MealDataService/Sources/Data/Remote/Response/MealMenuPerDayResponse.swift
+++ Services/MealDataService/Sources/Data/Remote/Response/MealMenuPerDayResponse.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 struct MealMenuPerDayResponse: Decodable {
-    let breakfast: [String]
-    let lunch: [String]
+    let breakfast: [String]?
+    let lunch: [String]?
     let dinner: [String]?
 }
 
@@ -12,9 +12,9 @@ extension MealMenuPerDayResponse {
         return .init(
             date: date,
             menu: [
-                .init(mealTime: .breakfast, menu: breakfast),
-                .init(mealTime: .lunch, menu: lunch),
-                .init(mealTime: .dinner, menu: dinner ?? [])
+                .init(mealTime: .breakfast, menu: breakfast ?? ["급식이 없습니다.1"]),
+                .init(mealTime: .lunch, menu: lunch ?? ["급식이 없습니다.2"]),
+                .init(mealTime: .dinner, menu: dinner ?? ["급식이 없습니다.3"])
             ]
         )
     }
