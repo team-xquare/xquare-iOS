ec5ab160f11bcd00edb8313f3d04b79e49eb3499
rlarldud1234 <rlarldud6482@naver.com>
2022-09-14 19:06:51 +0900

    ⚡️ :: UserResponse

---
+++ Services/UserService/Sources/Data/Remote/Response/UserResponse.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+import DateUtilModule
+
+struct UserResponse: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case id
+        case birthDay = "birth_day"
+        case grade
+        case classNumber = "class_num"
+        case profileImageUrlString = "profile_image_url"
+        case password
+        case accountId = "account_id"
+        case num
+    }
+    let name: String
+    let id: String
+    let birthDay: String
+    let grade: Int
+    let classNumber: Int
+    let profileImageUrlString: String
+    let password: String
+    let accountId: String
+    let num: Int
+
+}
+
+extension UserResponse {
+    func toDomain() -> UserEntity {
+        return .init(
+            name: name,
+            id: id,
+            birthDay: birthDay.toDate(format: .fullDate),
+            grade: grade,
+            classNumber: classNumber,
+            profileImageUrl: URL(string: profileImageUrlString)!,
+            password: password,
+            accountId: accountId,
+            num: num
+        )
+    }
+}
