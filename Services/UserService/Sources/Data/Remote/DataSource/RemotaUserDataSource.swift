a155af6c2ec4a9d23e111aee0609248d496b8866
rlarldud1234 <rlarldud6482@naver.com>
2022-09-14 19:05:23 +0900

    ⚡️ :: RemoteUserDataSource

---
+++ Services/UserService/Sources/Data/Remote/DataSource/RemotaUserDataSource.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+import RxSwift
+import RestApiModule
+
+protocol RemoteUserDataSource: RestApiRemoteDataSource<UserAPI> {
+    func fetchUserInformationById(userId: String) -> Single<UserEntity>
+    func fetchUserInformationByName(accountId: String) -> Single<UserEntity>
+    func fetchUserListByClass(grade: Int, classNumber: Int) -> Single<[ClassUserEntity]>
+    func fetchFCMToken(users: [String]) -> Single<FCMTokensEntity>
+    func fetchExcludedUserList(users: [String]) -> Single<UserIdsEntity>
+    func fetchUserPoint(users: [String]) -> Single<UserPointEntity>
+}
