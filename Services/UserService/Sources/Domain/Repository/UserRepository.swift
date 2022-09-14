f077ce49e6a281986bc9fd63bbfffa03bb42d820
rlarldud1234 <rlarldud6482@naver.com>
2022-09-14 19:08:04 +0900

    ⚡️ :: UserRepository

---
+++ Services/UserService/Sources/Domain/Repository/UserRepository.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+import RxSwift
+
+public protocol UserRepository {
+    func fetchUserInformationById(userId: String) -> Single<UserEntity>
+    func fetchUserInformationByName(accountId: String) -> Single<UserEntity>
+    func fetchUserListByClass(grade: Int, classNumber: Int) -> Observable<[ClassUserEntity]>
+    func fetchFCMToken(users: [String]) -> Single<FCMTokensEntity>
+    func fetchExcludedUserList(users: [String]) -> Single<UserIdsEntity>
+    func fetchUserPoint(users: [String]) -> Single<UserPointEntity>
+}
