98b47575b4c762c999a373de43f7dd4ff04feda5
rlarldud1234 <rlarldud6482@naver.com>
2022-09-16 19:59:23 +0900

    ⚡️ :: MealDetailView

---
+++ Application/Sources/Scene/MealDetail/MealDetailView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct MealDetailView: View {
+    @StateObject var viewModel: MealDetailViewModel
+
+    var body: some View {
+        NavigationView {
+            List(
+                viewModel.menu,
+                id: \.date
+            ) {
+                MealDetailCell(entity: $0)
+                    .listRowSeparator(.hidden)
+            }
+            .listStyle(.inset)
+            .navigationTitle("전체 급식")
+        }
+    }
+}
+
+struct MealDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        let viewModel = MealDetailViewModel()
+        MealDetailView(viewModel: viewModel)
+    }
+}
