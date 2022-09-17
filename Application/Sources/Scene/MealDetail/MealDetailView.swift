3c40548f8699e06e8ba33f0adb414e5aee98538c
rlarldud1234 <rlarldud6482@naver.com>
2022-09-17 21:45:34 +0900

    ⚰️ :: NavigationView 코드 삭제

--- Application/Sources/Scene/MealDetail/MealDetailView.swift
+++ Application/Sources/Scene/MealDetail/MealDetailView.swift
@@ -4,16 +4,15 @@ struct MealDetailView: View {
     @StateObject var viewModel: MealDetailViewModel
 
     var body: some View {
-        NavigationView {
-            List(
-                viewModel.menu,
-                id: \.date
-            ) {
-                MealDetailCell(entity: $0)
-                    .listRowSeparator(.hidden)
-            }
-            .navigationTitle("전체 급식")
+        List(
+            viewModel.menu,
+            id: \.date
+        ) {
+            MealDetailCell(entity: $0)
+                .listRowSeparator(.hidden)
         }
+        .navigationTitle("전체 급식")
+        .navigationBarTitleDisplayMode(.large)
     }
 }
 
