21553ae6b52bd0a86284ca169903d7a38ba31d03
kimxwan <kimxwan0319@naver.com>
2022-10-11 15:46:17 +0900

    📑 :: 필요한 Color들 추가

---
+++ Application/Sources/ResoucesCode/Color.swift
@@ -0,0 +1,6 @@
+import SwiftUI
+
+extension Color {
+    static let entireTabTopMenuBackgroundColor = Color("EntireTabTopMenuBackground")
+    static let entireTabTopMenuTextColor = Color("EntireTabTopMenuText")
+}
