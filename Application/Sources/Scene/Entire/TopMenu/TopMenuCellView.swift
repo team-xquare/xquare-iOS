a45d7d46bbe3a80cf83c4894f2601c177b62be8c
kimxwan <kimxwan0319@naver.com>
2022-10-11 15:47:10 +0900

    ⚡️ :: 전체탭 상단의 서비스 메뉴 뷰

--- Application/Sources/Scene/Entire/TopMenu/TopMenuCellView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TopMenuCellView.swift
-//  XQUARE-iOS
-//
-//  Created by 김수완 on 2022/10/11.
-//  Copyright © 2022 com.xquare. All rights reserved.
-//
-
-import Foundation
---
+++ Application/Sources/Scene/Entire/TopServiceMenu/TopServiceMenuView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct TopServiceMenuView: View {
+    var body: some View {
+        HStack(spacing: 9) {
+            TopServiceMenuCellView(image: .thumbUpIcon, text: "상벌점 내역") {
+                Text("상벌점 내역")
+            }
+            TopServiceMenuCellView(image: .userIcon, text: "마이페이지") {
+                Text("마이페이지")
+            }
+            TopServiceMenuCellView(image: .warningTriangleIcon, text: "버그 제보") {
+                Text("버그 제보")
+            }
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 11)
+        .padding(.bottom, 3)
+    }
+}
