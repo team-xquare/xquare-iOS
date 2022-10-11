aa9f99fdde3e9455a676b315a41b954cf61744fb
kimxwan <kimxwan0319@naver.com>
2022-10-11 14:03:39 +0900

    📑 :: 필요한 아이콘들 추가

---
+++ Application/Sources/Scene/Entire/MenuList/MenuSectionView.swift
@@ -0,0 +1,9 @@
+//
+//  MenuSectionView.swift
+//  XQUARE-iOS
+//
+//  Created by 김수완 on 2022/10/11.
+//  Copyright © 2022 com.xquare. All rights reserved.
+//
+
+import Foundation
---
+++ Application/Sources/Scene/Entire/TopMenu/TopMenuCellView.swift
@@ -0,0 +1,9 @@
+//
+//  TopMenuCellView.swift
+//  XQUARE-iOS
+//
+//  Created by 김수완 on 2022/10/11.
+//  Copyright © 2022 com.xquare. All rights reserved.
+//
+
+import Foundation
