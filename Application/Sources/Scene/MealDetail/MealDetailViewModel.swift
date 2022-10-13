e845471c939f558261c2dc35541997349d14cb22
rlarldud1234 <rlarldud6482@naver.com>
2022-10-13 14:18:44 +0900

    ⚡️ :: MealDetailViewModel

--- Application/Sources/Scene/MealDetail/MealDetailViewModel.swift
+++ Application/Sources/Scene/MealDetail/MealDetailViewModel.swift
@@ -1,7 +1,23 @@
 import Foundation
 
+import RxSwift
 import MealDataService
 
 class MealDetailViewModel: ObservableObject {
     @Published var menu: [MealMenuPerDayEntity] = []
+
+    private let fetchMealMenuPerMonthUseCase: FetchMealMenuPerMonthUseCase
+
+    init(fetchMealMenuPerMonthUseCase: FetchMealMenuPerMonthUseCase) {
+        self.fetchMealMenuPerMonthUseCase = fetchMealMenuPerMonthUseCase
+    }
+
+    private var disposeBag = DisposeBag()
+
+    func fetchMealMenuPerMonth() {
+        self.fetchMealMenuPerMonthUseCase.excute(date: Date())
+            .subscribe(onNext: {
+                self.menu = $0
+            }).disposed(by: disposeBag)
+    }
 }
