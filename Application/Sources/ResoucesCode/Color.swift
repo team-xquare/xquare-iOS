e761e26267364405dd95700aaa929e83d6a1eb9e
kimxwan <kimxwan0319@naver.com>
2022-10-25 08:45:19 +0900

    🐛 :: 에셋이름 중복 해결

--- Application/Sources/ResoucesCode/Color.swift
+++ Application/Sources/ResoucesCode/Color.swift
@@ -3,11 +3,11 @@ import SwiftUI
 extension Color {
 
     // Onboarding
-    static let onboardingDms = Color("Onboarding-DMS")
-    static let onboardingDnms = Color("Onboarding-DNMS")
-    static let onboardingDdyzd = Color("Onboarding-DDYZD")
-    static let onboardingJobits = Color("Onboarding-Jobits")
-    static let onboardingXquare = Color("Onboarding-Xquare")
+    static let onboardingDms = Color("Onboarding-DMS-Color")
+    static let onboardingDnms = Color("Onboarding-DNMS-Color")
+    static let onboardingDdyzd = Color("Onboarding-DDYZD-Color")
+    static let onboardingJobits = Color("Onboarding-Jobits-Color")
+    static let onboardingXquare = Color("Onboarding-Xquare-Color")
 
     static let entireTabTopMenuBackgroundColor = Color("EntireTabTopMenuBackground")
     static let entireTabTopMenuTextColor = Color("EntireTabTopMenuText")
