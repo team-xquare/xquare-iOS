88a218072b239d0ef4e1a848ddb979e735c8a3f7
rlarldud1234 <rlarldud6482@naver.com>
2022-10-24 11:55:49 +0900

    ⚡️ :: Entity Optional 처리

--- Services/MealDataService/Sources/Domain/Entity/MealMenuEntity.swift
+++ Services/MealDataService/Sources/Domain/Entity/MealMenuEntity.swift
@@ -3,9 +3,9 @@ import Foundation
 public struct MealMenuEntity: Equatable {
     public let mealTime: MealTime
     public let menu: [String]?
-    public let kcal: String
+    public let kcal: String?
 
-    public init(mealTime: MealTime, menu: [String]?, kcal: String) {
+    public init(mealTime: MealTime, menu: [String]?, kcal: String?) {
         self.mealTime = mealTime
         self.menu = menu
         self.kcal = kcal
