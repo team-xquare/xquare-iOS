6ddc9e9eb319c8398118275c119265c4021ba8ee
kimxwan <kimxwan0319@naver.com>
2022-10-25 00:38:45 +0900

    ⚡️ :: 온보딩 관련 이미지 및 컬러 리소스 코드 추가

--- Application/Sources/ResoucesCode/Color.swift
+++ Application/Sources/ResoucesCode/Color.swift
@@ -1,6 +1,15 @@
 import SwiftUI
 
 extension Color {
+
+    // Onboarding
+    static let onboardingDms = Color("Onboarding-DMS")
+    static let onboardingDnms = Color("Onboarding-DNMS")
+    static let onboardingDdyzd = Color("Onboarding-DDYZD")
+    static let onboardingJobits = Color("Onboarding-Jobits")
+    static let onboardingXquare = Color("Onboarding-Xquare")
+
     static let entireTabTopMenuBackgroundColor = Color("EntireTabTopMenuBackground")
     static let entireTabTopMenuTextColor = Color("EntireTabTopMenuText")
+
 }
