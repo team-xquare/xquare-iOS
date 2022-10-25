daa2c7a56ad15abc21046ce7d2c47928adbdea38
kimxwan <kimxwan0319@naver.com>
2022-10-25 11:32:51 +0900

    ⚡️ :: 오늘 급식부터 보여주기

--- Application/Sources/Scene/MealDetail/MealDetailViewModel.swift
+++ Application/Sources/Scene/MealDetail/MealDetailViewModel.swift
@@ -4,7 +4,9 @@ import RxSwift
 import MealDataService
 
 class MealDetailViewModel: ObservableObject {
+
     @Published var menu: [MealMenuPerDayEntity] = []
+    @Published var dateToShowData: Date = Date()
 
     private let fetchMealMenuPerMonthUseCase: FetchMealMenuPerMonthUseCase
 
@@ -16,8 +18,15 @@ class MealDetailViewModel: ObservableObject {
 
     func fetchMealMenuPerMonth() {
         self.fetchMealMenuPerMonthUseCase.excute(date: Date())
-            .subscribe(onNext: {
-                self.menu = $0
+            .subscribe(onNext: { [weak self] in
+                self?.menu = $0
+                self?.dateToShowData = self?.calculateDateToShowData(allMenu: $0) ?? Date()
             }).disposed(by: disposeBag)
     }
+
+    private func calculateDateToShowData(allMenu: [MealMenuPerDayEntity]) -> Date? {
+        let today = Calendar.current.startOfDay(for: Date())
+        return allMenu.first(where: { $0.date >= today })?.date ?? allMenu.last?.date
+    }
+
 }
