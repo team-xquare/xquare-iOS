8ceed39b221e5eb8b906b1a6a5016b0e8db7fc1c
kimxwan <kimxwan0319@naver.com>
2022-11-03 12:01:49 +0900

    📑 :: 파일 분할

--- Modules/XPhotos/Sources/XPhotoViewer/XPhotoViewer.swift
+++ Modules/XPhotos/Sources/XPhotoViewer/XPhotoViewer.swift
@@ -1,13 +1,5 @@
 import SwiftUI
 
-public extension View {
-    func xPhotoViewer(isPresented: Binding<Bool>, images: [URL]) -> some View {
-        self.fullScreenCover(isPresented: isPresented) {
-            XPhotoViewer(images: images)
-        }
-    }
-}
-
 public struct XPhotoViewer: View {
 
     @Environment(\.presentationMode) var presentationMode
