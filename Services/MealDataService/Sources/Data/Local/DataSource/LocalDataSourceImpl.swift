417cd53203e9f3682fa7d03b81b41efa8b8b6af7
rlarldud1234 <rlarldud6482@naver.com>
2022-11-06 21:49:21 +0900

    ⚡️ :: LocalDataSourceImpl

--- Services/MealDataService/Sources/Data/Local/DataSource/LocalDataSourceImpl.swift
+++ Services/MealDataService/Sources/Data/Local/DataSource/LocalDataSourceImpl.swift
@@ -6,35 +6,38 @@ class LocalDataSourceImpl: LocalDataSource {
 
     let mealDataSQLiteTask = MealDataServiceSQLiteTask.shared
 
-    init() {
-        mealDataSQLiteTask.createTable()
+    func registerMealMenuPerDay(menu: MealMenu) {
+        Task {
+            try await mealDataSQLiteTask.save(entity: menu)
+        }
     }
 
-    func registerMealMenuPerDay(menu: MealMenu) {
-        mealDataSQLiteTask.save(entity: menu)
+    func registerMealMenuPerMonth(menu: [MealMenu]) {
+        for item in menu {
+            Task {
+                try await self.mealDataSQLiteTask.save(entity: item)
+            }
+        }
     }
 
     func fetchMealMenuPerDay(day: Date) -> Single<MealMenu> {
         return Single.create { single -> Disposable in
-            single(.success(self.mealDataSQLiteTask.findMealByDay(day: day)))
-
+            Task {
+                let menu = await self.mealDataSQLiteTask.findMealByDay(day: day)
+                single(.success(menu))
+            }
             return Disposables.create()
         }
     }
 
     func fetchMealMenuPerMonth(day: Date) -> Single<[MealMenu]> {
         return Single.create { single -> Disposable in
-            single(.success(self.mealDataSQLiteTask.findMealByMonth(day: day)))
+            Task {
+                let menuList = await self.mealDataSQLiteTask.findMealByMonth(day: day)
+                single(.success(menuList))
+            }
 
             return Disposables.create()
-        }
-    }
-
-    func registerMealMenuPerMonth(menu: [MealMenu]) {
-        menu.forEach {
-            mealDataSQLiteTask.save(
-                entity: $0
-            )
         }
     }
 }
