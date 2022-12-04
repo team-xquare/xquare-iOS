3d6d9be2bba9181ef10e3542bdcb395dedf3de63
rlarldud1234 <rlarldud6482@naver.com>
2022-12-04 21:19:49 +0900

    ⚡️ :: LocalDataSourceImpl Task 내용 합침

--- Services/MealDataService/Sources/Data/Local/DataSource/LocalDataSourceImpl.swift
+++ Services/MealDataService/Sources/Data/Local/DataSource/LocalDataSourceImpl.swift
@@ -2,20 +2,49 @@ import Foundation
 
 import RxSwift
 
+import Boutique
+
 class LocalDataSourceImpl: LocalDataSource {
 
-    let mealDataSQLiteTask = MealDataServiceSQLiteTask.shared
+    let store = Store<MealMenu>(
+        storage: SQLiteStorageEngine(directory: .defaultStorageDirectory(appendingPath: "MealMenu"))!,
+        cacheIdentifier: \.day
+    )
 
     func registerMealMenuPerDay(menu: MealMenu) {
         Task {
-            try await mealDataSQLiteTask.save(entity: menu)
+            let mealMenu = MealMenu(
+                day: menu.day,
+                breakfast: menu.breakfast,
+                lunch: menu.lunch,
+                dinner: menu.dinner,
+                breakfastKcal: menu.breakfastKcal,
+                lunchKcal: menu.lunchKcal,
+                dinnerKcal: menu.dinner
+            )
+
+            try await store
+                .insert(mealMenu)
+                .run()
         }
     }
 
     func registerMealMenuPerMonth(menu: [MealMenu]) {
         for item in menu {
             Task {
-                try await self.mealDataSQLiteTask.save(entity: item)
+                let mealMenu = MealMenu(
+                    day: item.day,
+                    breakfast: item.breakfast,
+                    lunch: item.lunch,
+                    dinner: item.dinner,
+                    breakfastKcal: item.breakfastKcal,
+                    lunchKcal: item.lunchKcal,
+                    dinnerKcal: item.dinnerKcal
+                )
+
+                try await store
+                    .insert(mealMenu)
+                    .run()
             }
         }
     }
@@ -23,7 +52,20 @@ class LocalDataSourceImpl: LocalDataSource {
     func fetchMealMenuPerDay(day: Date) -> Single<MealMenu> {
         return Single.create { single -> Disposable in
             Task {
-                let menu = await self.mealDataSQLiteTask.findMealByDay(day: day)
+                let result = await self.store
+                    .items
+                    .filter {
+                        $0.day == day.toString(format: .fullDate)
+                    }
+                let menu = result.first ?? .init(
+                    day: Date().toString(format: .fullDate),
+                    breakfast: "",
+                    lunch: "",
+                    dinner: "",
+                    breakfastKcal: "",
+                    lunchKcal: "",
+                    dinnerKcal: ""
+                )
                 single(.success(menu))
             }
             return Disposables.create()
@@ -33,7 +75,12 @@ class LocalDataSourceImpl: LocalDataSource {
     func fetchMealMenuPerMonth(day: Date) -> Single<[MealMenu]> {
         return Single.create { single -> Disposable in
             Task {
-                let menuList = await self.mealDataSQLiteTask.findMealByMonth(day: day)
+                let menuList = await self.store
+                    .items
+                    .filter {
+                        $0.day.contains("\(day.toString(format: .year))-\(day.toString(format: .month))")
+                    }
+
                 single(.success(menuList))
             }
 
