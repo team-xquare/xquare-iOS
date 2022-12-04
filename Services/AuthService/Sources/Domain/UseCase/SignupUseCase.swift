e279084b71151578b2984aff374e4f5c68452805
rlarldud1234 <rlarldud6482@naver.com>
2022-12-04 19:48:09 +0900

    ⚡️ :: RxSwift to Combine

--- Services/AuthService/Sources/Domain/UseCase/SignupUseCase.swift
+++ Services/AuthService/Sources/Domain/UseCase/SignupUseCase.swift
@@ -1,17 +1,28 @@
 import Foundation
 
 import RxSwift
+import Combine
+import ComposableArchitecture
 
 public class SignupUseCase {
 
     private let authRepository: AuthRepository
+    private var disposeBag = DisposeBag()
 
     init(authRepository: AuthRepository) {
         self.authRepository = authRepository
     }
 
-    public func excute(data: SignupEntity) -> Completable {
-        self.authRepository.signup(signupEntity: data)
+    public func excute(data: SignupEntity) -> Future<Void, AuthServiceError> {
+        return Future<Void, AuthServiceError> { promise in
+            self.authRepository.signup(signupEntity: data)
+                .subscribe(onCompleted: {
+                    promise(.success(()))
+                }, onError: { error in
+                    promise(.failure(error as? AuthServiceError ?? .networkNotWorking))
+                })
+                .disposed(by: self.disposeBag)
+        }
     }
 
 }
