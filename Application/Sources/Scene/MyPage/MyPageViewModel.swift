ff834887854635fb02a907815c9e45d350225f04
rlarldud1234 <rlarldud6482@naver.com>
2022-12-05 20:13:33 +0900

    ⚡️ :: 변수명 이름 변경

--- Application/Sources/Scene/MyPage/MyPageViewModel.swift
+++ Application/Sources/Scene/MyPage/MyPageViewModel.swift
@@ -8,5 +8,5 @@ class MyPageViewModel: ObservableObject {
     @Published var birthDay: String = "2005.05.30"
     @Published var gradeClassNumber: String = "2학년 1반 3번"
     @Published var id: String = "rlarldud"
-    @Published var showActionSheet: Bool = false
+    @Published var xPhotosIsPresented: Bool = false
 }
