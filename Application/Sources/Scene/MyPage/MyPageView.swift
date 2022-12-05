a4ee225c709bd4d86c5715c5b62d4522db878f12
rlarldud1234 <rlarldud6482@naver.com>
2022-12-05 20:07:26 +0900

    ⚡️ :: MyPageView 퍼블리싱

--- Application/Sources/Scene/MyPage/MyPageView.swift
+++ Application/Sources/Scene/MyPage/MyPageView.swift
@@ -11,13 +11,32 @@ struct MyPageView: View {
             alignment: .center,
             spacing: 0
         ) {
+            Spacer()
+                .frame(height: 34)
             Image(uiImage: viewModel.profileImage)
+                .resizable()
+                .scaledToFit()
                 .frame(width: 60, height: 60)
-            SDTextField(title: "이름", text: $viewModel.name)
-            SDTextField(title: "생년월일", text: $viewModel.birthDay)
-            SDTextField(title: "학년 반 번호", text: $viewModel.gradeClassNumber)
-            SDTextField(title: "아이디", text: $viewModel.id)
+                .background(.black)
+                .cornerRadius(30)
+                .padding(.bottom, 6)
+            Button {
+                viewModel.showActionSheet = true
+            } label: {
+                Text("변경하기")
+                    .sdText(type: .body4, textColor: .GrayScale.gray900)
+            }
+            .padding(.bottom, 6)
+            MyInformationView(title: "이름", content: viewModel.name)
+            MyInformationView(title: "생년월일", content: viewModel.birthDay)
+            MyInformationView(title: "학년 반 번호", content: viewModel.gradeClassNumber)
+            MyInformationView(title: "아이디", content: viewModel.id)
+            Spacer()
         }
+        .padding(.horizontal, 16)
+        .setNavigationBackButton()
+        .navigationBarBackButtonHidden()
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationTitle("마이페이지")
     }
-
 }
--- Application/Sources/Scene/MyPage/MyPageViewModel.swift
+++ Application/Sources/Scene/MyPage/MyPageViewModel.swift
@@ -3,9 +3,10 @@ import Foundation
 import UIKit
 
 class MyPageViewModel: ObservableObject {
-    @Published var profileImage: UIImage = UIImage()
-    @Published var name: String = ""
-    @Published var birthDay: String = ""
-    @Published var gradeClassNumber: String = ""
-    @Published var id: String = ""
+    @Published var profileImage = UIImage()
+    @Published var name: String = "김기영"
+    @Published var birthDay: String = "2005.05.30"
+    @Published var gradeClassNumber: String = "2학년 1반 3번"
+    @Published var id: String = "rlarldud"
+    @Published var showActionSheet: Bool = false
 }
