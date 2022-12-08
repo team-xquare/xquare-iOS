3e774972b76ff54ff9edb369bdbbc3fbf34179cd
rlarldud1234 <rlarldud6482@naver.com>
2022-12-08 18:58:35 +0900

    ⚡️ :: AttachmentDataService 추가

--- Services/AttachmentService/Project.swift
+++ Services/AttachmentService/Project.swift
@@ -2,13 +2,9 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.framework(
-    name: "MealDataService",
+    name: "AttachmentService",
     platform: .iOS,
     isTestable: false,
     dependencies: [
-        .Module.RestApiMoudle,
-        .Module.XDateUtil,
-        .Module.XOfflineCache,
-        .SPM.Boutique
-    ]
-)
+        .Module.RestApiMoudle
+    ])
