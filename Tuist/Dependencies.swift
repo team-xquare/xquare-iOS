38799a38e0054d3fea5752905b5a44510e30a6ba
김수완 <kim.suwan@cashwalk.io>
2022-12-27 23:32:52 +0900

    ⚙️ :: SD; 업데이트 to 1.9.1

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -25,7 +25,7 @@ let dependencies = Dependencies(
                 requirement: .upToNextMajor(from: "6.5.0")),
         // SemicolonDesign
         .remote(url: "https://github.com/semicolonDSM/SemicolonDesign_iOS.git",
-                requirement: .upToNextMajor(from: "1.8.1"))
+                requirement: .upToNextMajor(from: "1.9.1"))
     ],
     baseSettings: Settings.settings(
         configurations: [
