0fe20ea6b3b9b240769878e85af3a23a8d9e0766
rlarldud1234 <rlarldud6482@naver.com>
2023-01-09 14:31:35 +0900

    ⚡️ :: DropDown

---
+++ Application/Sources/Scene/BugReport/DropDown/DropDown.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+import SemicolonDesign
+
+struct DropDown: View {
+    @Binding var text: String
+    var options: [String]
+    var optionSelectedAction: ((_ option: String) -> Void)?
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                ForEach(options, id: \.self) { option in
+                    DropDownRow(
+                        isSelected: .constant(text == option),
+                        option: option,
+                        optionSelectedAction: optionSelectedAction
+                    )
+                }
+            }
+            .cornerRadius(10)
+        }
+        .frame(height: CGFloat(options.count) * 40)
+    }
+}
