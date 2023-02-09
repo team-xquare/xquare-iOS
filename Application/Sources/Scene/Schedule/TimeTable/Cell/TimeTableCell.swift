7db2e5604b27930d2f10851739bea7eaf0432089
rlarldud1234 <rlarldud6482@naver.com>
2023-02-09 14:38:45 +0900

    ⚡️ :: TimeTableCell

---
+++ Application/Sources/Scene/Schedule/TimeTable/Cell/TimeTableCell.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+import SemicolonDesign
+
+struct TimeTableCell: View {
+    let period: Int
+
+    var body: some View {
+        HStack(alignment: .center, spacing: 0) {
+            Text("\(period)교시")
+                .sdText(type: .body1, textColor: .GrayScale.gray900)
+                .padding(.trailing, 12)
+            AsyncImage(url: URL(string: "")) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+            } placeholder: {
+                Color.GrayScale.gray100
+            }
+            .frame(width: 40, height: 40)
+            .cornerRadius(20)
+            .padding(.trailing, 12)
+            VStack(alignment: .leading, spacing: 1) {
+                Text("창체")
+                    .sdText(type: .body1, textColor: .GrayScale.gray900)
+                Text("08:40 ~ 09:30")
+                    .sdText(type: .caption, textColor: .GrayScale.gray600)
+            }
+        }
+    }
+}
