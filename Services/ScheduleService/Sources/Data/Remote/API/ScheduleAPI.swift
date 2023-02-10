2ac761c4d7c283e48b61221dd57f287769d475a1
rlarldud1234 <rlarldud6482@naver.com>
2023-02-10 15:10:24 +0900

    ⚡️ :: ScheduleAPI

---
+++ Services/ScheduleService/Sources/Data/Remote/API/ScheduleAPI.swift
@@ -0,0 +1,70 @@
+import Foundation
+
+import AuthService
+import Moya
+import RestApiModule
+
+enum ScheduleAPI {
+    case fetchScheduleForMonth(month: Int)
+    case createSchoolSchedule(name: String, date: String)
+    case createSchedule(name: String, date: String)
+}
+
+extension ScheduleAPI: XquareAPI {
+
+    var domain: String {
+        return "/schedules"
+    }
+
+    var urlPath: String {
+        switch self {
+        case .fetchScheduleForMonth, .createSchoolSchedule:
+            return "/school"
+        default:
+            return ""
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .fetchScheduleForMonth(let month):
+            return .requestParameters(
+                parameters: [
+                    "month": month
+                ],
+                encoding: URLEncoding.default
+            )
+        case .createSchoolSchedule(let name, let date), .createSchedule(let name, let date):
+            return .requestParameters(
+                parameters: [
+                    "name": name,
+                    "date": date
+                ],
+                encoding: JSONEncoding.default
+            )
+        }
+    }
+
+    var errorMapper: [Int: Error]? {
+        return [
+            401: ScheduleServiceError.unauthorized,
+            408: ScheduleServiceError.timeOut,
+            409: ScheduleServiceError.conflict,
+            429: ScheduleServiceError.tooManyRequests
+        ]
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .fetchScheduleForMonth:
+            return .get
+        default:
+            return .post
+        }
+    }
+
+    var jwtTokenType: JWTTokenType {
+        return .accessToken
+    }
+
+}
