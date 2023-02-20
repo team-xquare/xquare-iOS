942042137699256b771440c4b7cf946d4759ec9e
rlarldud1234 <rlarldud6482@naver.com>
2023-02-20 16:19:59 +0900

    ⚡️ :: PageControl

--- Application/Sources/Scene/Schedule/TimeTable/TimeTableView.swift
+++ Application/Sources/Scene/Schedule/TimeTable/TimeTableView.swift
@@ -5,26 +5,34 @@ import TimeTableService
 
 struct TimeTableView: View {
     @StateObject var viewModel: TimeTableViewModel
+    @State var weekDay: Int = 1
 
     var body: some View {
         VStack(spacing: 0) {
             Spacer()
                 .frame(height: 16)
-            TabView {
+            TabView(selection: $weekDay) {
                 ForEach(viewModel.timeTable, id: \.weekDay) { weekTimeTable in
-                    Text(weekTimeTable.date.toString(format: "MM월 dd일 (E)"))
-                        .sdText(type: .body2, textColor: .GrayScale.gray900)
-                        .padding(.bottom, 16)
-                    List(weekTimeTable.dayTimeTable, id: \.period) { timeTable in
-                        TimeTableCell(timeTable: timeTable)
-                            .listRowSeparator(.hidden)
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text(weekTimeTable.date.toString(format: "MM월 dd일 (E)"))
+                            .sdText(type: .body2, textColor: .GrayScale.gray900)
+                            .padding(.bottom, 16)
+                        List(weekTimeTable.dayTimeTable, id: \.period) { timeTable in
+                            TimeTableCell(timeTable: timeTable)
+                                .listRowSeparator(.hidden)
+                                .listRowInsets(EdgeInsets())
+                                .padding(.bottom, 18)
+                        }
+                        .listStyle(.plain)
                     }
-                    .listStyle(.inset)
                 }
             }
+            .padding(.horizontal, 24)
             .tabViewStyle(.page)
+
+            PageControlView(weekDay: $weekDay)
+                .padding(.vertical, 20)
         }
-        .padding(.horizontal, 24)
         .onAppear(perform: viewModel.fetchTimeTable)
     }
 }
