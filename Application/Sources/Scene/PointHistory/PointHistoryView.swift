de26ae6028bb3dd03a74bac742e6ca18d8fcc20a
rlarldud1234 <rlarldud6482@naver.com>
2023-03-03 11:01:33 +0900

    ⚡️ :: @State -> @StateObject로 변경

--- Application/Sources/Scene/PointHistory/PointHistoryView.swift
+++ Application/Sources/Scene/PointHistory/PointHistoryView.swift
@@ -5,12 +5,12 @@ import SemicolonDesign
 
 struct PointHistoryView: View {
 
-    @State var viewModel: PointHistoryViewModel
+    @StateObject var viewModel: PointHistoryViewModel
     @State var isGoodPointButtonSelected: Bool = true
     @State var isBadPointButtonSelected: Bool = false
 
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 0) {
 
             Text("상점 \(viewModel.pointHistories.goodPoint)점 벌점 \(viewModel.pointHistories.badPoint)점")
                 .sdText(type: .body4)
@@ -18,36 +18,27 @@ struct PointHistoryView: View {
 
             HStack {
                 SelectButtonView(selection: $isGoodPointButtonSelected, text: "상점") {
-                    self.viewModel.type = true
                     self.isBadPointButtonSelected = false
-                }
-                .onChange(of: isGoodPointButtonSelected) {
-                    if $0 {
-                        self.viewModel.fetchPointHistory()
-                    }
+                    self.viewModel.fetchPointHistory(type: .goodPoint)
                 }
                 SelectButtonView(selection: $isBadPointButtonSelected, text: "벌점") {
-                    self.viewModel.type = false
                     self.isGoodPointButtonSelected = false
-                }
-                .onChange(of: isBadPointButtonSelected) {
-                    if $0 {
-                        self.viewModel.fetchPointHistory()
-                    }
+                    self.viewModel.fetchPointHistory(type: .badPoint)
                 }
             }
             .padding([.horizontal, .vertical], 16)
 
             List(viewModel.pointHistories.pointHistories, id: \.id) {
                 PointHistoryListCellView(pointHistory: $0)
-                    .padding(.horizontal, 16)
+                    .listRowSeparator(.hidden)
             }
+            .listStyle(.inset)
 
         }
         .navigationTitle("상벌점 내역")
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarBackButtonHidden()
         .setNavigationBackButton()
-        .onAppear(perform: viewModel.fetchPointHistory)
+        .onAppear { viewModel.fetchPointHistory(type: .goodPoint) }
     }
 }
