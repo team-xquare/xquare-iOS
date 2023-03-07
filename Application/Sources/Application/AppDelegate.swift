65a0acd157639300aac9f66244b0651c756bdb2b
rlarldud1234 <rlarldud6482@naver.com>
2023-03-08 02:17:25 +0900

    ⚡️ :: 세로모드로 고정하기

--- Application/Sources/Application/AppDelegate.swift
+++ Application/Sources/Application/AppDelegate.swift
@@ -57,4 +57,11 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         completionHandler()
     }
 
+    func application(
+        _ application: UIApplication,
+        supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
+        // 세로방향 고정
+        return UIInterfaceOrientationMask.portrait
+    }
 }
