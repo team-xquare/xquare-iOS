d837a2df04db60ff61b93dd5d6166acd20e148b1
rlarldud1234 <rlarldud6482@naver.com>
2023-03-13 00:48:12 +0900

    ⚡️ :: keychain sharing

--- Services/AuthService/Sources/Data/Local/DataSource/LoaclTokenDataSourceImpl.swift
+++ Services/AuthService/Sources/Data/Local/DataSource/LoaclTokenDataSourceImpl.swift
@@ -7,6 +7,10 @@ class LoaclTokenDataSourceImpl: LoaclTokenDataSource {
 
     let keychain = KeychainModule<KeychainTypeForAuth>()
 
+    init() {
+        keychain.accessGroup = "5G56CAU886.com.xquare.XQUARE-iOS.keychainGroup"
+    }
+
     func registerAccessToken(_ accessToken: String) {
         self.keychain.set(accessToken, forKey: .accessToken)
     }
