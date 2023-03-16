a27b4bf425f05777feb5059e923e7d87f9e7edd5
juyeong <juyeongno1@naver.com>
2023-03-16 17:19:56 +0900

    ⚡️ :: Dependency

---
+++ Services/PickService/Sources/DI/PickServiceDependency.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+public struct PickServiceDependency {
+
+    public static let shared = Self.resolve()
+
+    public let fetchOutingPassUseCase: FetchOutingPassUseCase
+    public let fetchMovedClassUseCase: FetchMovedClassUseCase
+    public let fetchOutingReturnTimeUseCase: FetchOutingReturnTimeUseCase
+
+}
+
+extension PickServiceDependency {
+    static func resolve() -> PickServiceDependency {
+        // MARK: - Datasources
+        let remotePickDataSource: RemotePickDataSource = RemotePointDataSourceImpl()
+
+        // MARK: - Respositories
+        let pickRepository: PickRepository = PickRepositoryImpl(
+            remoteDataSource: remotePickDataSource
+        )
+
+        // MARK: - UseCases
+        let fetchOutingPassUseCase = FetchOutingPassUseCase(
+            pickRepository: pickRepository
+        )
+        let fetchMovedClassUseCase = FetchMovedClassUseCase(
+            pickRepository: pickRepository
+        )
+        let fetchOutingReturnTimeUseCase = FetchOutingReturnTimeUseCase(
+            pickRepository: pickRepository
+        )
+
+
+        return PickServiceDependency(
+            fetchOutingPassUseCase: fetchOutingPassUseCase,
+            fetchMovedClassUseCase: fetchMovedClassUseCase,
+            fetchOutingReturnTimeUseCase: fetchOutingReturnTimeUseCase
+        )
+    }
+}
