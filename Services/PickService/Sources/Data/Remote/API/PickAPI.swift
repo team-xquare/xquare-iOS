3ddbd795c118f5db1edbab54588d68334f1ce38a
juyeong <juyeongno1@naver.com>
2023-03-16 17:10:13 +0900

    ⚡️ :: API

---
+++ Services/PickService/Sources/Data/Remote/API/PickAPI.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+import AuthService
+import Moya
+import RestApiModule
+
+enum PickAPI {
+    case fetchOutingReturnTime
+    case fetchMovedClass
+    case fetchOutingPass
+}
+
+extension PickAPI: XquareAPI {
+
+    var domain: String {
+        return "/pick"
+    }
+
+    var urlPath: String {
+        switch self {
+        case .fetchOutingReturnTime:
+            return "/applications/return"
+        case .fetchMovedClass:
+            return "/class-room/location"
+        case .fetchOutingPass:
+            return "/applications/picnic"
+        }
+    }
+
+    var task: Task {
+        return .requestPlain
+    }
+
+    var errorMapper: [Int: Error]? {
+        return [
+            400: PickServiceError.badRequest,
+            401: PickServiceError.unauthorized,
+            404: PickServiceError.notFound
+        ]
+    }
+
+    var method: Moya.Method {
+        return .get
+    }
+
+    var jwtTokenType: JWTTokenType {
+        return .accessToken
+    }
+
+}
