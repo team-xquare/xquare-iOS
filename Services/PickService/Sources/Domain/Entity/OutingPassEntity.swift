c2cda8ec08d4cadd4905fc4643bc599dbdc8833a
juyeong <juyeongno1@naver.com>
2023-03-16 18:57:58 +0900

    ⚡️ :: Entity

--- Services/PickService/Sources/Domain/Entity/OutingPassEntity.swift
+++ Services/PickService/Sources/Domain/Entity/OutingPassEntity.swift
@@ -4,13 +4,13 @@ public struct OutingPassEntity: Equatable {
     private let profileFileName: String
     private let studentNumber: String
     private let studentName: String
-    private let startTime: String
-    private let endTime: String
+    private let startTime: Date
+    private let endTime: Date
     private let reason: String
     private let teacherName: String
 
-    public init(profileFileName: String, studentNumber: String, studentName: String, startTime: String,
-                endTime: String, reason: String, teacherName: String) {
+    public init(profileFileName: String, studentNumber: String, studentName: String, startTime: Date,
+                endTime: Date, reason: String, teacherName: String) {
         self.profileFileName = profileFileName
         self.studentNumber = studentNumber
         self.studentName = studentName
--- Services/PickService/Sources/Domain/Entity/OutingReturnTimeEntity.swift
+++ Services/PickService/Sources/Domain/Entity/OutingReturnTimeEntity.swift
@@ -3,9 +3,9 @@ import Foundation
 public struct OutingReturnTimeEntity: Equatable {
     public let userId: String
     public let name: String
-    public let endTime: String
+    public let endTime: Date
 
-    public init(userId: String, name: String, endTime: String) {
+    public init(userId: String, name: String, endTime: Date) {
         self.userId = userId
         self.name = name
         self.endTime = endTime
