e9906574419b5dec029b9952b2aca90be77e8922
Byung jin <bjcho1503@naver.com>
2023-03-20 08:30:44 +0900

    ⚡️ :: [HomeViewModel] 외출증, 교실이동 기능 추가

--- Application/Sources/Scene/Home/HomeViewModel.swift
+++ Application/Sources/Scene/Home/HomeViewModel.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 import MealDataService
 import UserService
+import PickService
 import RxSwift
 
 class HomeViewModel: ObservableObject {
@@ -13,16 +14,31 @@ class HomeViewModel: ObservableObject {
     @Published var merit: Int = 0
     @Published var demerit: Int = 0
     @Published var menu: [MealMenuEntity] = []
+    @Published var endTime: Date = Date()
+    @Published var locationClassroom: String = ""
+    @Published var isShowOutingView: Bool = false
+    @Published var isShowMovedClass: Bool = false
+    @Published var isPresentErrorAlert: Bool = false
+    @Published var isSuccessReturnClass: Bool = false
 
     private let fetchMealMenuPerDayUseCase: FetchMealMenuPerDayUseCase
     private let fetchUserPointUseCase: FetchUserSimpleInformationUseCase
+    private let fetchOutingReturnTimeUseCase: FetchOutingReturnTimeUseCase
+    private let fetchMovedClassUseCase: FetchMovedClassUseCase
+    private let deleteReturnClassUseCase: DeleteReturnClassUseCase
 
     init(
         fetchMealMenuPerDayUseCase: FetchMealMenuPerDayUseCase,
-        fetchUserPointUseCase: FetchUserSimpleInformationUseCase
+        fetchUserPointUseCase: FetchUserSimpleInformationUseCase,
+        fetchOutingReturnTimeUseCase: FetchOutingReturnTimeUseCase,
+        fetchMovedClassUseCase: FetchMovedClassUseCase,
+        deleteReturnClassUseCase: DeleteReturnClassUseCase
     ) {
         self.fetchMealMenuPerDayUseCase = fetchMealMenuPerDayUseCase
         self.fetchUserPointUseCase = fetchUserPointUseCase
+        self.fetchOutingReturnTimeUseCase = fetchOutingReturnTimeUseCase
+        self.fetchMovedClassUseCase = fetchMovedClassUseCase
+        self.deleteReturnClassUseCase = deleteReturnClassUseCase
     }
 
     func fetchTodaysMeal() {
@@ -44,4 +60,34 @@ class HomeViewModel: ObservableObject {
             }).disposed(by: disposeBag)
     }
 
+    func fetchOutingPass() {
+        self.fetchOutingReturnTimeUseCase.excute()
+            .subscribe(onNext: {
+                self.name = $0.name
+                self.endTime = $0.endTime
+                self.isShowOutingView = true
+            })
+            .disposed(by: disposeBag)
+    }
+
+    func fetchMovedClass() {
+        self.fetchMovedClassUseCase.excute()
+            .subscribe(onNext: {
+                self.name = $0.name
+                self.locationClassroom = $0.locationClassroom
+                self.isShowMovedClass = true
+            })
+            .disposed(by: disposeBag)
+    }
+
+    func deleteReturnClass() {
+        self.deleteReturnClassUseCase.excute()
+            .subscribe(onCompleted: {
+                self.isSuccessReturnClass = true
+                self.isShowMovedClass = false
+            }, onError: { _ in
+                self.isPresentErrorAlert = true
+            })
+            .disposed(by: disposeBag)
+    }
 }
