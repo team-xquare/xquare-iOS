11cdeecb9af1adef7c11387465bcf0c3d2e784b3
rlarldud1234 <rlarldud6482@naver.com>
2023-03-20 00:43:37 +0900

    ⚡️ :: 변수명 변경

--- Services/NotificationService/Sources/Data/Remote/Response/NotificationResponse.swift
+++ Services/NotificationService/Sources/Data/Remote/Response/NotificationResponse.swift
@@ -7,7 +7,7 @@ struct NotificationResponse: Decodable {
         case id
         case title
         case content
-        case sendAt = "send_at"
+        case sendAt = "sent_at"
         case isRead = "is_read"
         case userId = "user_id"
         case name
@@ -29,7 +29,7 @@ extension NotificationResponse {
             id: id,
             title: title,
             content: content,
-            sendAt: sendAt.toDate(format: .fullDate),
+            sendAt: sendAt.toDate(format: .fullDateWithTime),
             isRead: isRead,
             userId: userId,
             name: name,
