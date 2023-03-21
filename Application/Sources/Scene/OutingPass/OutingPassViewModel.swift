9c448f4dafd442ca0b176bffd03f0c8c055cdc6e
Byung jin <bjcho1503@naver.com>
2023-03-21 21:44:17 +0900

    ⚡️ :: viewModel 변경

--- Application/Sources/Scene/OutingPass/OutingPassViewModel.swift
+++ Application/Sources/Scene/OutingPass/OutingPassViewModel.swift
@@ -4,6 +4,15 @@ import RxSwift
 import PickService
 
 class OutingPassViewModel: ObservableObject {
+    @Published var outingPassData: OutingPassEntity = .init(
+        profileFileName: "",
+        studentNumber: "",
+        studentName: "",
+        startTime: Date(),
+        endTime: Date(),
+        reason: "",
+        teacherName: ""
+    )
 
     private let fetchOutingPassUseCase: FetchOutingPassUseCase
 
@@ -13,22 +22,10 @@ class OutingPassViewModel: ObservableObject {
 
     private var disposeBag = DisposeBag()
 
-    @Published var name: String = ""
-    @Published var studentNumber: String = ""
-    @Published var imageUrl: String = ""
-    @Published var time: String = ""
-    @Published var resone: String = ""
-    @Published var teacher: String  = ""
-
     func fetchOutingPass() {
         self.fetchOutingPassUseCase.excute()
             .subscribe(onNext: {
-                self.name = $0.studentName
-                self.studentNumber = $0.studentNumber
-                self.imageUrl = $0.profileFileName
-                self.time = "\($0.startTime.toString(format: "HH:mm")) ~ \($0.endTime.toString(format: "HH:mm"))"
-                self.resone = $0.reason
-                self.teacher = $0.teacherName
+                self.outingPassData = $0
             })
             .disposed(by: disposeBag)
     }
