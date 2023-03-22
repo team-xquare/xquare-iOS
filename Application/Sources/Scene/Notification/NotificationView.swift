0cc9fa6d6acb95d7851c9ba124d5aeabd64e9dd8
rlarldud1234 <rlarldud6482@naver.com>
2023-03-22 11:40:25 +0900

    ⚡️ :: Notirication 띄워주기

--- Application/Sources/Scene/Notification/NotificationView.swift
+++ Application/Sources/Scene/Notification/NotificationView.swift
@@ -9,12 +9,10 @@ struct NotificationView: View {
         VStack(spacing: 0) {
             List(viewModel.notifications, id: \.id) {
                 NotificationListCell(
-                    image: .init(systemName: "checkmark.circle"),
-                    category: $0.name,
-                    content: $0.title,
-                    time: $0.sendAt.toString(format: "HH")
+                    entity: $0
                 )
                 .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets())
             }
             .listStyle(.inset)
         }
