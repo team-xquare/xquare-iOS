9d69c423fe7f723e321ce204adb0af6e41865caa
Byung jin <bjcho1503@naver.com>
2023-03-28 22:17:06 +0900

    ⚡️ :: Repository에 자습선생님 조회 추가

--- Services/PickService/Sources/Domain/Repository/PickRepository.swift
+++ Services/PickService/Sources/Domain/Repository/PickRepository.swift
@@ -6,5 +6,6 @@ public protocol PickRepository {
     func fetchOutingReturnTime() -> Observable<OutingReturnTimeEntity>
     func fetchOutingPass() -> Observable<OutingPassEntity>
     func fetchMovedClass() -> Observable<MovedClassEntity>
+    func fetchTodaySelfStudyTeacher() -> Observable<TodaySelfStudyTeacherEntity>
     func deleteReturnClass() -> Completable
 }
