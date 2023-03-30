281b8f9dc814124805a3913a2ec1e799ffa3682a
juyeong525 <juyeongno1@naver.com>
2023-03-30 20:05:24 +0900

    ⚡️ :: ServiceSectionView

--- Application/Sources/Scene/Entire/ServiceList/ServiceSectionView.swift
+++ Application/Sources/Scene/Entire/ServiceList/ServiceSectionView.swift
@@ -1,17 +1,17 @@
 import SwiftUI
 
-struct ServiceSectionView: View {
+struct ServiceSectionView<Destination>: View where Destination: View {
 
     var headerText: String
-    var services: [String]
+    var services: [(text: String, destination: Destination)]
 
     var body: some View {
         VStack(alignment: .leading, spacing: 11) {
             Text(headerText)
                 .sdText(type: .body1, textColor: .GrayScale.gray900)
-            ForEach(services, id: \.self) { service in
-                ServiceListCellView(text: service) {
-                    Text(service)
+            ForEach(services, id: \.text) { service in
+                ServiceListCellView(text: service.text) {
+                    service.destination
                 }
             }
         }
