d43ac6774d3e1feebd1774e5b4261ccfb5d6b179
juyeong525 <juyeongno1@naver.com>
2023-03-31 11:46:03 +0900

    ⚡️ :: SelfStduyTeacherViewModel

---
+++ Application/Sources/Scene/SelfStudyTeacher/SelfStudyTeacherViewModel.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+import RxSwift
+
+class SelfStudyTeacherViewModel: ObservableObject {
+
+    @Published var dateToShowData: Int?
+
+    func dateToShowDatas() {
+        self.dateToShowData = calculateDateToShowData()
+    }
+    private var disposeBag = DisposeBag()
+
+    private func calculateDateToShowData() -> Int {
+        let today = Calendar.current.startOfDay(for: Date())
+        return Int(today.toString(format: "d")) ?? 0
+    }
+
+}
