22f6da6c5491922c03ed1a0e04006a2791058e10
rlarldud1234 <rlarldud6482@naver.com>
2023-04-02 22:03:13 +0900

    ⚡️ :: Typealias XquareRouter

---
+++ Application/Sources/Router/Route/Router/XquareRouter.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+typealias OnboardingRouter = Router<OnboardingRoute, OnboardingRouterFactory>
+typealias HomeRouter = Router<HomeRoute, HomeRouterFactory>
+typealias ScheduleRouter = Router<ScheduleRoute, ScheduleRouterFactory>
+typealias FeedRouter = Router<FeedRoute, FeedRouterFactory>
+typealias ApplicationRouter = Router<ApplicationRoute, ApplicationRouterFactory>
+typealias EntireRouter = Router<EntireRoute, EntireRouterFactory>
