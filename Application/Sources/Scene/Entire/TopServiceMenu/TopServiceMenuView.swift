4644867ad4ac791267c6ff28f7b1b9db867aa7a1
Byung jin <bjcho1503@naver.com>
2023-04-12 22:42:53 +0900

    ⚡️ :: 버그제보 클릭시 페이스북으로 이동하는 기능추가

--- Application/Sources/Scene/Entire/TopServiceMenu/TopServiceMenuView.swift
+++ Application/Sources/Scene/Entire/TopServiceMenu/TopServiceMenuView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct TopServiceMenuView: View {
+    @Environment(\.openURL) var openURL
     @EnvironmentObject var xquareRouter: XquareRouter
 
     var body: some View {
@@ -12,7 +13,7 @@ struct TopServiceMenuView: View {
                 self.xquareRouter.navigateTo(.myPage)
             }
             TopServiceMenuCellView(image: .warningTriangleIcon, text: "버그 제보") {
-                self.xquareRouter.navigateTo(.bugReport)
+                openURL(URL(string: "https://www.facebook.com/profile.php?id=100091948951498")!)
             }
         }
         .padding(.top, 20)
