8f01f9529a227680be17f4add643c56c0f2578ea
juyeong525 <juyeongno1@naver.com>
2023-04-17 10:22:04 +0900

    ⚡️ :: BugAPI

---
+++ Services/BugService/Sources/Data/Remote/API/BugAPI.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+import AuthService
+import Moya
+import RestApiModule
+
+enum BugAPI {
+    case postBugReport(request: PostBugRequest)
+}
+
+extension BugAPI: XquareAPI {
+    var domain: String {
+        return "/reports"
+    }
+
+    var urlPath: String {
+        return ""
+    }
+
+    var method: Moya.Method {
+        return .post
+    }
+
+    var errorMapper: [Int: Error]? {
+        return [
+            400: BugServiceError.badRequest,
+            401: BugServiceError.unauthorized,
+            403: BugServiceError.forbidden,
+            404: BugServiceError.notFound,
+            407: BugServiceError.proxyAuthenticationRequired,
+            408: BugServiceError.requestTimeout,
+            409: BugServiceError.conflict,
+            429: BugServiceError.tooManyRequests,
+            500: BugServiceError.internalServerError
+        ]
+    }
+
+    var task: Task {
+        switch self {
+        case .postBugReport(let request):
+            return .requestJSONEncodable(request)
+        }
+    }
+
+    var jwtTokenType: JWTTokenType {
+        return .accessToken
+    }
+
+}
