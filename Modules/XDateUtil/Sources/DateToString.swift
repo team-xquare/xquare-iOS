9bfc635eaa7ae969ded62db67eb05c0e86cc6928
juyeong525 <juyeongno1@naver.com>
2023-04-18 23:11:03 +0900

    ⚡️ :: XDateUtil

--- Modules/XDateUtil/Sources/DateToString.swift
+++ Modules/XDateUtil/Sources/DateToString.swift
@@ -8,7 +8,6 @@ public extension Date {
 
     func toString(format: String) -> String {
         XDateFormatter.shared.dateFormat = format
-        XDateFormatter.shared.timeZone = TimeZone(identifier: "ko_KR")
         return XDateFormatter.shared.string(from: self)
     }
 
