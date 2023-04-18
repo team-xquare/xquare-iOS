806f8b6130304bba8e28533d76d00008fadd2e6d
juyeong525 <juyeongno1@naver.com>
2023-04-19 00:00:57 +0900

    🐛 :: XDateUtil 튕김 버그 수정

--- Modules/XDateUtil/Sources/StringToDate.swift
+++ Modules/XDateUtil/Sources/StringToDate.swift
@@ -3,7 +3,6 @@ import Foundation
 public extension String {
     func toDate(format: DateFormat) -> Date {
         XDateFormatter.shared.dateFormat = format.rawValue
-        XDateFormatter.shared.timeZone = TimeZone(identifier: "KST")
         return XDateFormatter.shared.date(from: self)!
     }
 }
