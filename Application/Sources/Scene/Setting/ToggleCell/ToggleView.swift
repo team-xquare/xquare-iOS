79ce3316b42a54b09d4b88b14dfa8e93921a1d62
juyeong525 <juyeongno1@naver.com>
2023-05-11 22:43:49 +0900

    ⚡️ :: ToggleCell

---
+++ Application/Sources/Scene/Setting/ToggleCell/ToggleView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+import SemicolonDesign
+
+struct ToggleView: View {
+
+    var title: String
+    var subTitle: String
+    @Binding var isToggle: Bool
+    var body: some View {
+        HStack(alignment: .center) {
+            VStack(alignment: .leading, spacing: 0) {
+                Text(title)
+                    .sdText(type: .body1, textColor: .GrayScale.gray900)
+                Text(subTitle)
+                    .sdText(type: .caption, textColor: .GrayScale.gray500)
+            }
+            Spacer()
+            Toggle("", isOn: $isToggle)
+                .toggleStyle(SwitchToggleStyle(tint: .Primary.purple400))
+                .labelsHidden()
+        }
+        .padding(.horizontal, 16)
+        .padding(.bottom, 24)
+    }
+}
