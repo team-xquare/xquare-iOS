bf213ab7efbb8c19d1242902a2dcb64dc7ce98de
Byung jin <bjcho1503@naver.com>
2023-05-12 23:02:43 +0900

    ⚡️ :: DateFormat case 추가

--- Modules/XDateUtil/Sources/DateFormat.swift
+++ Modules/XDateUtil/Sources/DateFormat.swift
@@ -1,8 +1,10 @@
 import Foundation
 
 public enum DateFormat: String {
+    /// yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS
+    case fullDateWithMilliSecondTime = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS"
     /// yyyy-MM-dd'T'HH:mm:ss
-    case fullDateWithTime = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS"
+    case fullDateWithTime = "yyyy-MM-dd'T'HH:mm:ss"
     /// yyyy-MM-dd
     case fullDate = "yyyy-MM-dd"
     /// yyyy
