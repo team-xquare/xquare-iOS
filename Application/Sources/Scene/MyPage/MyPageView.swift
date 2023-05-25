b636203641fae52fa5cf2c58178fe38baa3ff609
juyeong525 <juyeongno1@naver.com>
2023-05-26 01:12:39 +0900

    ♻️ :: 디자인 변경에 따라 코드 수정

--- Application/Sources/Scene/MyPage/MyPageView.swift
+++ Application/Sources/Scene/MyPage/MyPageView.swift
@@ -12,23 +12,31 @@ struct MyPageView: View {
             alignment: .center,
             spacing: 0
         ) {
-            Spacer()
-                .frame(height: 34)
-            ProfileImageView(
-                imageUrl: viewModel.profileImagrUrl,
-                uiimage: $viewModel.profileImage
-            )
-            Button {
-                viewModel.xPhotosIsPresented = true
-            } label: {
-                Text("변경하기")
-                    .sdText(type: .body4, textColor: .GrayScale.gray900)
+            Spacer().frame(height: 20)
+            HStack(alignment: .center, spacing: 16) {
+                AsyncImage(url: viewModel.profileImagrUrl) { image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                } placeholder: {
+                    Image("DefaultImage")
+                }
+                .frame(width: 60, height: 60)
+                .cornerRadius(30)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text(viewModel.name)
+                        .sdText(type: .body1, textColor: .GrayScale.gray900)
+                    Text(viewModel.gradeClassNum)
+                        .sdText(type: .body2, textColor: .GrayScale.gray900)
+                }
+                Spacer()
             }
-            .padding(.bottom, 6)
-            MyInformationView(title: "이름", content: viewModel.name)
-            MyInformationView(title: "생년월일", content: viewModel.birthDay)
-            MyInformationView(title: "학년 반 번호", content: viewModel.gradeClassNum)
+            .padding(.bottom, 20)
+            .padding(.leading, 4)
             MyInformationView(title: "아이디", content: viewModel.id)
+            MyInformationView(title: "생년월일", content: viewModel.birthDay)
+//            MyInformationView(title: "깃허브 계정", content: viewModel.id)
+            Spacer()
             Button(action: {
                 viewModel.showLogoutAlert = true
             }, label: {
@@ -39,12 +47,8 @@ struct MyPageView: View {
                     .background(Color.System.red50)
                     .cornerRadius(8)
             })
-            Spacer()
+            .padding(.bottom, 40)
         }
-        .sdPhotoPicker(
-            isPresented: $viewModel.xPhotosIsPresented,
-            selection: $viewModel.profileImage
-        )
         .sdAlert(isPresented: $viewModel.showLogoutAlert) {
             SDAlert(
                 title: "정말 회원탈퇴 하시겠습니까?",
@@ -54,6 +58,16 @@ struct MyPageView: View {
                     self.xquareRouter.popToRoot()
                 })
             )
+        }
+        .toolbar {
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                Button {
+                    self.xquareRouter.navigateTo(.editMyPage)
+                } label: {
+                    Text("수정")
+                        .sdText(type: .body1)
+                }
+            }
         }
         .onChange(of: viewModel.profileImage, perform: { _ in
             viewModel.uploadImage()
