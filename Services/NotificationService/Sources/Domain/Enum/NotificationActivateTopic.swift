fe42036d4feaf87c67e85303a8d9954cd79b1c83
Byung jin <bjcho1503@naver.com>
2023-05-27 01:29:48 +0900

    ⚡️ :: 알림 토픽 enum추가

---
+++ Services/NotificationService/Sources/Domain/Enum/NotificationActivateTopic.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public enum NotificationActivateTopic: String {
+    case FEED = "FEED"
+    case APPLICATION = "APPLICATION"
+    case ALL = "ALL"
+    case SCHEDULE = "SCHEDULE"
+}
