36a2e1bea509904453387584aa202f454bbe7c0f
Byung jin <bjcho1503@naver.com>
2023-06-01 09:03:36 +0900

    ⚡️ :: 버튼 글자 색 변경 가능하도록 수정

--- Application/Sources/Scene/Entire/ServiceList/ServiceSectionButtonView.swift
+++ Application/Sources/Scene/Entire/ServiceList/ServiceSectionButtonView.swift
@@ -2,19 +2,25 @@ import SwiftUI
 
 import SemicolonDesign
 
+// swiftlint: disable large_tuple
 struct ServiceSectionButtonView: View {
 
     var headerText: String
-    var services: [(text: String, action: () -> Void)]
+    var services: [(text: String, action: () -> Void, textColor: Color)]
 
     var body: some View {
         VStack(alignment: .leading, spacing: 11) {
             Text(headerText)
                 .sdText(type: .body1, textColor: .GrayScale.gray900)
             ForEach(services, id: \.0) { service in
-                ServiceListCellButtonView(text: service.text, action: service.action)
+                ServiceListCellButtonView(
+                    text: service.text,
+                    action: service.action,
+                    textColor: service.textColor
+                )
             }
         }
         .padding(.horizontal, 16)
     }
 }
+// swiftlint: enable large_tuple
