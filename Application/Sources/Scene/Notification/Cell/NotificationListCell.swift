571bfaee18aa756a65e85da47d5fbe2aedf332d7
juyeong525 <juyeongno1@naver.com>
2023-06-03 20:53:11 +0900

    ⚡️ :: getTimeAgoAsKoreanString 사용

--- Application/Sources/Scene/Notification/Cell/NotificationListCell.swift
+++ Application/Sources/Scene/Notification/Cell/NotificationListCell.swift
@@ -21,7 +21,7 @@ struct NotificationListCell: View {
                         .sdText(type: .caption2)
 
                     Spacer()
-                    Text(getTimeForSend(date: entity.sendAt))
+                    Text(entity.sendAt.getTimeAgoAsKoreanString())
                         .sdText(type: .caption2)
                 }
                 Text(entity.content)
@@ -33,26 +33,6 @@ struct NotificationListCell: View {
         .padding(16)
         .background(entity.isRead ? .white : .GrayScale.gray50)
     }
-
-    private func getTimeForSend(date: Date) -> String {
-            let today = Date()
-            let resultMinute = calculateToMinute(date: today) - calculateToMinute(date: date)
-            if resultMinute / 60 == 0 && resultMinute % 60 == 0 {
-                return "방금 전"
-            } else if resultMinute / 60 == 0 {
-                return "\(resultMinute % 60)분 전"
-            } else if resultMinute / 60 / 24 == 0 {
-                return "\(resultMinute / 60 % 24)시간 전"
-            } else if resultMinute / 60 / 24 / 30 == 0 {
-                return "\(resultMinute / 60 / 24 % 30)일 전"
-            } else {
-                return "\(resultMinute / 60 / 24 / 30)달 전"
-            }
-        }
-        private func calculateToMinute(date: Date) -> Int {
-            let dateList = date.toString(format: "MM:dd:HH:mm").split(separator: ":").map { Int($0)! }
-            return dateList[0] * 30 * 24 * 60 * 1 + dateList[1] * 24 * 60 * 1 + dateList[2] * 60 * 1 + dateList[3]
-        }
     // swiftlint:disable line_length
     private func topicToImage(topic: NotificationTopic) -> Image {
         switch topic {
