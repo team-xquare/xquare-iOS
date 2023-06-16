6ca02f91151837f481dde42d6edd4fee497c1dc7
juyeong525 <juyeongno1@naver.com>
2023-06-16 11:46:43 +0900

    ⚡️ :: 사진 넣으면 defaultImageView 안보이게 처리

--- Application/Sources/Scene/BugReport/BugImage/BugImageView.swift
+++ Application/Sources/Scene/BugReport/BugImage/BugImageView.swift
@@ -16,26 +16,28 @@ struct BugImageView: View {
         } else {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 16) {
-                    ZStack(alignment: .center) {
-                        Spacer()
-                        Image.defaultBugImage
-                    }
-                    .frame(width: 160, height: 160)
-                    .background(Color.GrayScale.gray100)
-                    .cornerRadius(8)
-                    .padding(.bottom, 24)
-                    .onTapGesture {
-                        xPhotosIsPresented = true
-                    }
                     if !isEmpty {
                         ForEach(uiimage, id: \.self) {
                             Image(uiImage: $0)
                                 .resizable()
+                                .scaledToFill()
                                 .frame(width: 160, height: 160)
                                 .cornerRadius(8)
                                 .padding(.bottom, 24)
                         }
+                    } else {
+                        ZStack(alignment: .center) {
+                            Spacer()
+                            Image.defaultBugImage
+                        }
+                        .frame(width: 160, height: 160)
+                        .background(Color.GrayScale.gray100)
+                        .cornerRadius(8)
+                        .padding(.bottom, 24)
                     }
+                }
+                .onTapGesture {
+                    xPhotosIsPresented = true
                 }
             }
         }
