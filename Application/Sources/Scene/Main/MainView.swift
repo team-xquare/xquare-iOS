8fd6765c112c720a482797f26fdaab1e594188da
juyeong525 <juyeongno1@naver.com>
2023-06-19 19:45:42 +0900

    ⚡️ :: case 추가

--- Application/Sources/Scene/Main/MainView.swift
+++ Application/Sources/Scene/Main/MainView.swift
@@ -27,6 +27,9 @@ struct MainView: View {
             entireView
         )}
         .onReceive(activeNotification, perform: {
+            if xquareRouter.stack.count > 2 {
+                xquareRouter.dismissLast()
+            }
             let categoryIdentifier = NotificationTopic(rawValue: ($0.userInfo!["categoryIdentifier"] as? String ?? ""))
             if let index = topicToPage(topic: categoryIdentifier!).0 {
                 self.xquareRouter.moveTabTo(index: index)
@@ -36,6 +39,9 @@ struct MainView: View {
             } else { return }
         })
         .onReceive(backgroundNotification, perform: { _ in
+            if xquareRouter.stack.count > 2 {
+                xquareRouter.dismissLast()
+            }
             let userDefault = UserDefaults.standard
             if let categoryIdentifier = NotificationTopic(rawValue: userDefault.string(forKey: "categoryIdentifier")!) {
                 if let index = topicToPage(topic: categoryIdentifier).0 {
@@ -53,8 +59,24 @@ struct MainView: View {
 
 private func topicToPage(topic: NotificationTopic) -> (Int?, XquareRoute?) {
     switch topic {
-    case .allBadPoint, .allGoodPoint, .allPenaltyLevel:
-        return (0, .pointHistory)
+    case .allGoodPoint, .allPenaltyLevel, .allBadPoint:
+        return (4, .pointHistory)
+    case .feedNoticeLike, .feedBambooLike, .feedBambooComment, .feedNoticeComment:
+        return (2, nil)
+    case .applicationStay, .applicationPicnicPass, .applicationWeekendMeal,
+            .applicationWeekendPicnic, .applicationWeekendPicnicReservation:
+        return (3, nil)
+    case .applicationPicnic:
+        return (0, .outingPass)
+    case .applicationMoveClassRoom:
+        return (0, nil)
+    case .scheduleLocal, .scheduleSocial:
+        NotificationCenter.default.post(
+            name: NSNotification.Name("schedule"),
+            object: nil,
+            userInfo: ["isSchedule": false]
+        )
+        return (1, nil)
     default:
         return (nil, nil)
     }
