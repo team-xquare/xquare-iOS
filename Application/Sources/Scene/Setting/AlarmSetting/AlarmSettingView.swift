d8b61e6b9eadb323971c6907a9748c2712dec301
Byung jin <bjcho1503@naver.com>
2023-06-29 19:02:35 +0900

    ⚡️ :: 신청 버튼 주석처리

--- Application/Sources/Scene/Setting/AlarmSetting/AlarmSettingView.swift
+++ Application/Sources/Scene/Setting/AlarmSetting/AlarmSettingView.swift
@@ -19,11 +19,11 @@ struct AlarmSettingView: View {
                 subTitle: "댓글 또는 좋아요가 달렸을 때 보내요.",
                 isToggle: $isFeedToggle
             )
-            ToggleView(
-                title: "신청",
-                subTitle: "급식, 외출 신청 등의 알림이에요.",
-                isToggle: $isApplicationToggle
-            )
+//            ToggleView(
+//                title: "신청",
+//                subTitle: "급식, 외출 신청 등의 알림이에요.",
+//                isToggle: $isApplicationToggle
+//            )
             ToggleView(
                 title: "상벌점",
                 subTitle: "상점 또는 벌점에 변화가 있을 때 보내요.",
