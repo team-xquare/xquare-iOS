a79840c9f4bfcddf3a1d05c951984ea45e22cbb4
Byung jin <bjcho1503@naver.com>
2023-08-18 15:59:15 +0900

    ⚡️ :: 깃허브 연동 안했을때 분기처리

--- Application/Sources/Scene/GitHubRanking/ListCell/RankingListCellView.swift
+++ Application/Sources/Scene/GitHubRanking/ListCell/RankingListCellView.swift
@@ -9,26 +9,35 @@ struct RankingListCellView: View {
 
     var body: some View {
         HStack(alignment: .center) {
-            AsyncImage(url: URL(string: myGithubData?.profileFileName ?? "")) { image in
-                image
-                    .resizable()
-                    .scaledToFill()
-            } placeholder: {
-                Image("DefaultImage")
-                    .resizable()
-                    .scaledToFill()
+            if let data = myGithubData {
+                AsyncImage(url: URL(string: myGithubData?.profileFileName ?? "")) { image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                } placeholder: {
+                    Image("DefaultImage")
+                        .resizable()
+                        .scaledToFill()
+                }
+                .frame(width: 42, height: 42)
+                .cornerRadius(21)
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("\(myGithubData?.ranking ?? 0)위 : \(myGithubData?.name ?? "")")
+                        .sdText(type: .caption2, textColor: .GrayScale.gray900)
+                    Text("@\(myGithubData?.username ?? "")")
+                        .sdText(type: .caption3, textColor: .GrayScale.gray700)
+                }
+                Spacer()
+                Text("\(myGithubData?.contributions ?? 0) 커밋")
+                    .sdText(type: .body1, textColor: .GrayScale.gray900)
+            } else {
+                HStack {
+                    Text("깃허브 연동이 필요합니다.")
+                        .sdText(type: .body3, textColor: .GrayScale.gray900)
+                    Spacer()
+                }
+                .padding(.vertical, 10)
             }
-            .frame(width: 42, height: 42)
-            .cornerRadius(21)
-            VStack(alignment: .leading, spacing: 2) {
-                Text("\(myGithubData?.ranking ?? 0)위 : \(myGithubData?.name ?? "")")
-                    .sdText(type: .caption2, textColor: .GrayScale.gray900)
-                Text("@\(myGithubData?.username ?? "")")
-                    .sdText(type: .caption3, textColor: .GrayScale.gray700)
-            }
-            Spacer()
-            Text("\(myGithubData?.contributions ?? 0) 커밋")
-                .sdText(type: .body1, textColor: .GrayScale.gray900)
         }
         .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 15))
         .background(Color.GrayScale.gray50)
