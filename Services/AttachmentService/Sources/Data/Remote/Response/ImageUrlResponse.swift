548ac42b4137f5c907dfeb63a45fd7374e286a29
Byung jin <bjcho1503@naver.com>
2023-08-29 17:16:25 +0900

    ⚰️ :: 필요없는 파일 삭제

--- Services/AttachmentService/Sources/Data/Remote/Response/ImageUrlResponse.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct ImageUrlResponse: Decodable {
-    private enum CodingKeys: String, CodingKey {
-        case imageUrlString = "fileUrl"
-    }
-    let imageUrlString: [String]
-}
