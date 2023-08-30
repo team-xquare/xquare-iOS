a98929ca92e7034126152d0a1cd06ebe929a87f1
juyeong525 <juyeongno1@naver.com>
2023-08-30 17:02:59 +0900

    ♻️ :: LocalMealDataSourceImpl

---
+++ Services/MealDataService/Sources/Data/Local/DataSource/LocalMealDataSourceImpl.swift
@@ -0,0 +1,96 @@
+import Foundation
+
+import RxSwift
+import XDateUtil
+import Boutique
+
+class LocalMealDataSourceImpl: LocalMealDataSource {
+
+    let store = Store<MealMenu>(
+        storage: SQLiteStorageEngine(directory: .defaultStorageDirectory(appendingPath: "MealMenu"))!,
+        cacheIdentifier: \.day
+    )
+
+    func registerMealMenuPerDay(menu: MealMenu) {
+        Task {
+            do {
+                let mealMenu = MealMenu(
+                    day: menu.day,
+                    breakfast: menu.breakfast,
+                    lunch: menu.lunch,
+                    dinner: menu.dinner,
+                    breakfastKcal: menu.breakfastKcal,
+                    lunchKcal: menu.lunchKcal,
+                    dinnerKcal: menu.dinnerKcal
+                )
+                try await store
+                    .insert(mealMenu)
+                    .run()
+            } catch {
+                print("Error inserting meal menu: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    func registerMealMenuPerMonth(menu: [MealMenu]) {
+        for item in menu {
+            Task {
+                do {
+                    let mealMenu = MealMenu(
+                        day: item.day,
+                        breakfast: item.breakfast,
+                        lunch: item.lunch,
+                        dinner: item.dinner,
+                        breakfastKcal: item.breakfastKcal,
+                        lunchKcal: item.lunchKcal,
+                        dinnerKcal: item.dinnerKcal
+                    )
+                    try await store
+                        .insert(mealMenu)
+                        .run()
+                } catch {
+                    print("Error inserting meal menu: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+
+    func fetchMealMenuPerDay(day: Date) -> Single<MealMenu> {
+        return Single.create { single -> Disposable in
+            Task {
+                let result = await self.store
+                    .items
+                    .filter {
+                        $0.day == day.toString(format: .fullDate)
+                    }
+                let menu = result.first ?? .init(
+                    day: Date().toString(format: .fullDate),
+                    breakfast: "",
+                    lunch: "",
+                    dinner: "",
+                    breakfastKcal: "",
+                    lunchKcal: "",
+                    dinnerKcal: ""
+                )
+                single(.success(menu))
+            }
+            return Disposables.create()
+        }
+    }
+
+    func fetchMealMenuPerMonth(day: Date) -> Single<[MealMenu]> {
+        return Single.create { single -> Disposable in
+            Task {
+                let menuList = await self.store
+                    .items
+                    .filter {
+                        $0.day.contains("\(day.toString(format: .year))-\(day.toString(format: .month))")
+                    }
+
+                single(.success(menuList))
+            }
+
+            return Disposables.create()
+        }
+    }
+}
