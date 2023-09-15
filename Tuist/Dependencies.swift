da260625f422c51245e07d1b789e8f833d391d79
Byung jin <bjcho1503@naver.com>
2023-09-15 09:48:30 +0900

    ⚡️ :: 디자인 시스템 버전 수정

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -25,7 +25,7 @@ let dependencies = Dependencies(
                 requirement: .upToNextMajor(from: "6.5.0")),
         // SemicolonDesign
         .remote(url: "https://github.com/semicolonDSM/SemicolonDesign_iOS.git",
-                requirement: .upToNextMajor(from: "1.14.2")),
+                requirement: .upToNextMajor(from: "1.14.3")),
         // XWebKit
         .remote(url: "https://github.com/team-xquare/XWebKit",
                 requirement: .upToNextMajor(from: "1.2.5"))
